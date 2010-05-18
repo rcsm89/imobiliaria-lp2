9d70710382af8120476e94a0be5ed45a4b53e711
brunofpaiwa <brunofpaiwa@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-18 19:49:39 +0000

    Segue a classe Cliente.

---
+++ trunk/iMobiliaria/src/imobiliaria/processamento/Cliente.java
@@ -0,0 +1,91 @@
+package imobiliaria.processamento;
+
+import java.util.Calendar;
+
+/**
+ * Objetos desta classe sao clientes da imobiliaria
+ * 
+ * @author Bruno Fabio de Farias Paiva
+ * @version IT01
+ */
+public class Cliente extends Pessoa {
+
+	private ColecaoImoveis historicoCompras;
+	private ColecaoImoveis pedidos;
+	private TipoImovel preferencia;
+
+	/**
+	 * Construtor responsavel por criar um novo cliente
+	 * 
+	 * @param cpf
+	 *            Representa o cadastro de pessoa fisica de um cliente
+	 * @param dataNascimento
+	 *            Representa a data de nascimento de um cliente
+	 * @param endereco
+	 *            Representa o endereco de um cliente
+	 * @param nome
+	 *            Representa o nome de um cliente
+	 */
+	public Cliente(String cpf, Calendar dataNascimento, String endereco,
+			String nome, TipoImovel preferencia) {
+		super(cpf, dataNascimento, endereco, nome);
+		this.preferencia = preferencia;
+	}
+
+	/**
+	 * Metodo acessador ao historico de compras de um cliente
+	 * 
+	 * @return O historico das compras de um cliente
+	 */
+	public ColecaoImoveis getHistoricoCompras() {
+		return historicoCompras;
+	}
+
+	/**
+	 * Metodo acessador aos pedidos de um cliente
+	 * 
+	 * @return Os pedidos que um cliente efetuou
+	 */
+	public ColecaoImoveis getColecao() {
+		return pedidos;
+	}
+
+	/**
+	 * Metodo acessador a preferencia do cliente
+	 * 
+	 * @return Tipo de imovel que o cliente prefere
+	 */
+	public TipoImovel getPreferencia() {
+		return preferencia;
+	}
+
+	/**
+	 * Metodo modificador da preferencia de um cliente
+	 * 
+	 * @param preferencia
+	 *            Representa a nova preferencia do cliente
+	 */
+	public void setPreferencia(TipoImovel preferencia) {
+		this.preferencia = preferencia;
+	}
+
+	/* (non-Javadoc)
+	 * @see imobiliaria.processamento.Pessoa#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object objeto) {
+
+		if (!(objeto instanceof Cliente)) {
+			return false;
+		}
+
+		Cliente outroCliente = (Cliente) objeto;
+
+		if (getCpf() != outroCliente.getCpf()) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
