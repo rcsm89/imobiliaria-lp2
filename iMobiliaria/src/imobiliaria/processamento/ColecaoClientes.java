df90118bcba192d2895ab02ab9807d41a5ec2822
brunofpaiwa <brunofpaiwa@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-19 04:08:45 +0000

    Classe colecaoClientes, incompleta.!

---
+++ iMobiliaria/src/imobiliaria/processamento/ColecaoClientes.java
@@ -0,0 +1,145 @@
+//ATENCAO GET CLIENTE POR PEDIDOS - INDECISO! CLASSE INCOMPLETA!
+package imobiliaria.processamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe ColecaoClientes que guarda clientes
+ * 
+ * @version IT01
+ */
+public class ColecaoClientes {
+
+	// Atributos
+
+	private List<Cliente> colecaoClientes = new ArrayList<Cliente>();
+
+	/**
+	 * Metodo que adiciona clientes na Colecao
+	 * 
+	 * @param clienteASerAdicionado
+	 *            cliente que sera Adicionado
+	 * @return True - Caso o cliente tenha sido adicionado <br>
+	 *         False - Caso o cliente ja exista na colecao
+	 */
+	public boolean adicionaCliente(Cliente clienteASerAdicionado) {
+
+		if (colecaoClientes.contains(clienteASerAdicionado)) {
+			return false;
+		}
+
+		return colecaoClientes.add(clienteASerAdicionado);
+	}
+
+	/**
+	 * Metodo que remove um cliente da Colecao
+	 * 
+	 * @param cpfDoClienteASerRemovido
+	 *            Representa o CPF do cliente a Ser Removido
+	 * @return True - Caso o cliente seja removido <br>
+	 *         False - Caso ele nao seja encontrado
+	 */
+	public boolean removeCliente(String cpfDoClienteASerRemovido) {
+		final int TAM_CPF_FORMATADO = 11;
+
+		if (VerificaInvalido.basico(cpfDoClienteASerRemovido)) {
+			return false;
+		}
+		if (VerificaInvalido.numeroFormatado(cpfDoClienteASerRemovido,
+				TAM_CPF_FORMATADO)) {
+			return false;
+		}
+
+		for (Cliente cliente : colecaoClientes) {
+			if (cliente.getCpf() == cpfDoClienteASerRemovido) {
+				return colecaoClientes.remove(cliente);
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Metodo acessador de toda a colecao de clientes
+	 * 
+	 * @return ArrayList contendo todos os clientes da Colecao
+	 */
+	public List<Cliente> getClientes() {
+		return colecaoClientes;
+	}
+
+	/**
+	 * Metodo responsavel por retornar a quantidade de clientes
+	 * 
+	 * @return Inteiro, indicando o total de clientes cadastrados
+	 */
+	public int numeroTotalDeClientes() {
+		return colecaoClientes.size();
+	}
+
+	/**
+	 * Metodo responsavel por retornar os clientes com determinada preferencia
+	 * 
+	 * @param preferencia
+	 * 			Representa a preferencia dos clientes que serao retornados
+	 * 
+	 * @return Clientes com a dada preferencia de imovel passada
+	 */
+	public List<Cliente> getClientesPorPreferencia(TipoImovel preferencia){
+
+		List<Cliente> colecaoRetornada = new ArrayList<Cliente>();
+
+		for (Cliente cliente: colecaoClientes) {
+
+			if (cliente.getPreferencia() == preferencia) {
+				colecaoRetornada.add(cliente);
+			}
+		}
+		return colecaoRetornada;
+	}
+
+	/**
+	 * Metodo filtrador que retorna clientes com mesmos pedidos de imovel
+	 * 
+	 * @param tipoDeImovelRequerido
+	 *            Tipo de Imovel para ser usado como Filtro
+	 * @return ArrayList contendo imoveis do determinado Tipo de Imovel
+	 */
+	public List<Cliente> getClientePorPedidos(ColecaoImoveis pedidoRequerido) {
+
+		List<Cliente> colecaoRetornada = new ArrayList<Cliente>();
+
+		for (Cliente cliente: colecaoClientes) {
+			
+			for (Imovel pedido: cliente.getPedidos().getImoveis()){
+				
+				if (pedido.equals(pedidoRequerido))
+					colecaoRetornada.add(cliente);
+			}
+		}
+		return colecaoRetornada;
+
+	}
+
+	/**
+	 * Metodo filtrador que retorna clientes com um determinado nome.
+	 * 
+	 * @param nome
+	 *            Representa o nome pelo qual seram pesquisados os novos
+	 *             clientes 
+	 * @return List contendo os clientes que possuam o nome passado
+	 */
+	public List<Cliente> getClientesPorNome(String nome) {
+
+		List<Cliente> colecaoRetornada = new ArrayList<Cliente>();
+
+		for (Cliente cliente : colecaoClientes) {
+
+			if (cliente.getNome().contains(nome)) {
+				colecaoRetornada.add(cliente);
+			}
+		}
+		return colecaoRetornada;
+	}
+
+}
