906821b3fb1e34ec7887fdb1e150f9807721b2fe
yurifariasg <yurifariasg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-21 02:18:28 +0000

    Main Atualizado

--- iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
+++ iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
@@ -1,21 +1,21 @@
 package imobiliaria.processamento;
 
-
 import imobiliaria.aux.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ImobiliariaMain {
-	
-//	private static Sistema sistema = new Sistema();
-	
-//	private static Scanner sc = new Scanner(System.in);
-	
+
+	private static Sistema sistema = new Sistema();
+
+
 	public static void main(String[] args) {
-		
+
 		boolean repete = true;
 		int opcao = 0;
+		
 		String promptCliente = 	
 			" * ----------"
 			+ "------------- * Imobiliaria * ----------------------- * \n"
@@ -34,13 +34,14 @@ public class ImobiliariaMain {
 			opcao = recebeInteiroEntre(0, 4);
 
 			switch (opcao) {
-			
+
 			case 1:
-				
+
 				System.out.println("Digite seu Login: ");
 				String loginAdmin = VerificaInvalido.recebeString();
+				System.out.println("Digite sua Senha: ");
 				String senhaAdmin = VerificaInvalido.recebeString();
-				
+
 				if (!(loginAdmin.equals("admin") && senhaAdmin.equals("admin"))) {
 					System.out.println("Login e/ou Senha invalida!");
 				} else {
@@ -48,16 +49,16 @@ public class ImobiliariaMain {
 				}
 
 				break;
-				
+
 			case 2:
-				
+
 				System.out.println("Func AEW!");
 
 			case 3:
-				
-				try{	
+
+				try {
 					criaCliente();
-				}catch(Exception e){
+				} catch (Exception e) {
 					System.out.println(e.getMessage());
 					repete = false;
 					break;
@@ -67,10 +68,10 @@ public class ImobiliariaMain {
 				break;
 
 			case 4:
-				
+
 				System.out.println("Cadastra Cliente");
 				break;
-				
+
 			case 0:
 
 				repete = false;
@@ -84,7 +85,7 @@ public class ImobiliariaMain {
 		} while (repete);
 		System.out.println("Programa Finalizado");
 	}
-	
+
 	
 	private static String promptPrincipal() {
 		return " * ----------"
@@ -99,11 +100,11 @@ public class ImobiliariaMain {
 	}
 
 	private static Cliente criaCliente() {
-		//cpf = recebe(cpf);
-		//nascimento = recebe(calendar - data);
-		//endereco = recebe(endereco);
-		//nome = recebe(nome);
-		//preferencia = rece(TipoImovel.CASA);
+		// cpf = recebe(cpf);
+		// nascimento = recebe(calendar - data);
+		// endereco = recebe(endereco);
+		// nome = recebe(nome);
+		// preferencia = rece(TipoImovel.CASA);
 		Calendar nascimento = new GregorianCalendar(1991, Calendar.APRIL, 4);
 		String endereco = "Rua alBerto de brito, 844";
 		TipoImovel preferencia = TipoImovel.CASA;
@@ -118,32 +119,103 @@ public class ImobiliariaMain {
 
 	
 	/* Metodos de Administrador */
-	
-	
+
+	// SuppressWarnings pelo Cast do Object para HashMap
+	@SuppressWarnings("unchecked")
 	private static void opcoesAdmin() {
-		System.out.println("\n" +
-				"Menu de Administracao - iMobiliaria" +
-				"1. ");
+		
+		boolean menuAdminRodando = true;
+		
+		System.out.println("\n" + "Menu de Administracao - iMobiliaria\n"
+				+ "1. Verificar e Alterar dados de Clientes\n"
+				+ "2. Verificar e Alterar dados de Funcionarios\n"
+				+ "3. Verificar e Alterar dados de Imoveis\n"
+				+ "4. Efetua pedido de Cliente\n"
+				+ "5. Pagamento de Contas\n"
+				+ "6. Sair");
+		
+		int opcaoEscolhida = recebeInteiroEntre(1, 6);
+
+		switch (opcaoEscolhida) {
+
+		case 1:
+			// CRDU de Clientes
+			//crduClientes();
+			break;
+		case 2:
+			// CRDU de Funcioanrios
+			//crduFuncionarios();
+			break;
+		case 3:
+			// CRDU de Imoveis
+			//crduImoveis();
+			break;
+		case 4:
+			// Efetua pedido do Cliente
+			break;
+		case 5:
+
+			// Se ele ja pagou esse Mes
+			Object[] folhaDePagamento;
+			
+			try {
+				
+				folhaDePagamento = sistema.efetuaPagamentoNoMes();
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				break;
+			}
+
+			HashMap<String, Double> pagamentoFuncionarios = (HashMap<String, Double>) folhaDePagamento[0];
+			
+			if (!(pagamentoFuncionarios.isEmpty())) {
+				System.out.println("Funcionario ---------- Salario");
+				for (String funcionario : pagamentoFuncionarios.keySet()) {
+					System.out.println(funcionario + " ------- "
+							+ pagamentoFuncionarios.get(funcionario));
+				}
+			} else {
+				System.out.println("Nenhum funcionario cadastrado");
+			}
+			
+			System.out.println("-------------------------------------------\n" +
+					"Total Antigo : " + (Double) folhaDePagamento[1] + "\n" +
+							"Despesas: " + (Double) folhaDePagamento[2] + "\n" +
+									"Novo Total: " + sistema.getCaixaTotal());
+			
+			break;
+		case 6:
+			menuAdminRodando = false;
+			break;
+		}
+		
+		/* Se ele nao digitou para sair (opcao 6)
+		 * O menu eh chamado novamente (Chamada Recursiva)
+		 */
+		
+		if (menuAdminRodando) {
+			opcoesAdmin();
+		}
 	}
-	
-	
+
 	/* Metodos Auxiliares ou de Entrada */
 	
 	private static int recebeInteiroEntre(int min, int max) {
-	      Scanner entrada = new Scanner(System.in);
-	      if (!entrada.hasNextInt()) {
-	         entrada.next();
-	         System.out.println("Numero invalido! Digite novamente: ");
-	         return recebeInteiroEntre(min, max);
-	      }
-	      
-	      int numero = entrada.nextInt();
+		Scanner entrada = new Scanner(System.in);
+		if (!entrada.hasNextInt()) {
+			entrada.next();
+			System.out.println("Numero invalido! Digite novamente: ");
+			return recebeInteiroEntre(min, max);
+		}
 
+		int numero = entrada.nextInt();
+		
 	      if (numero < min || numero > max) {
 	         System.out.println("Numero invalido! Digite novamente: ");
 	         return recebeInteiroEntre(min, max);
 	      }
 	      return numero;
-	  } 
+	}
 
 }
