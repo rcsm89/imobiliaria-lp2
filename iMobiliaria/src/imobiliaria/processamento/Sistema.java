521bc20eb22bf4cff3779457276a7594fd3820b6
yurifariasg <yurifariasg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-21 00:38:43 +0000

    Classe Sistema Terminada :D

--- iMobiliaria/src/imobiliaria/processamento/Sistema.java
+++ iMobiliaria/src/imobiliaria/processamento/Sistema.java
@@ -5,8 +5,16 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 /**
- *
+ * Classe Sistema para guardar informacoes de um Sistema Imobiliario
+ * 
+ * @author Yuri
+ * @version IT 1
  */
+
+ /*
+  * Essa Classe irá guardar todas as informacoes do Sistema, tais como banco de dados
+  * contendo os Imoveis, Clientes, Funcionarios e o Caixa da Empresa
+  */
 public class Sistema {
 
 	private final double SALARIO_DEFAULT = 500;
@@ -19,6 +27,8 @@ public class Sistema {
 	private HashMap<String, String> loginFuncionarios = new HashMap<String, String>();
 	private double caixaTotal;
 
+	private Object[] pagouNesseMes = { new GregorianCalendar(2010, 06, 1), true };
+
 	/*
 	 * pagouNesseMes = [DataDoUltimoPagamento, JaPagouNesseMes]
 	 * 
@@ -26,7 +36,16 @@ public class Sistema {
 	 * foi feito na data de 01/06/2010 :D
 	 */
 
-	private Object[] pagouNesseMes = { new GregorianCalendar(2010, 06, 1), true };
+	/**
+	 * Metodo para efetuar pagamento no Mes
+	 * 
+	 * @return Array de Object Contendo:<br>
+	 *         Hashmap contendo <Funcionario, SeuSalario><br>
+	 *         Double contendo o Caixa Antigo Total<br>
+	 *         Double contendo as Despesas do Pagamento
+	 * @throws Exception
+	 *             Lanca excecao se ja tiver feito pagamento esse mes
+	 */
 
 	public Object[] efetuaPagamentoNoMes() throws Exception {
 
@@ -38,36 +57,43 @@ public class Sistema {
 			throw new Exception("Pagamento ja efetuado esse mes");
 		}
 
-		HashMap<String, Double> folhaDePagamentoFuncionarios = new HashMap<String, Double>();
-		
+		HashMap<String, Double> salarioFuncionarios = new HashMap<String, Double>();
+
 		double despesas = 0;
 		double salarioFuncionario;
 
 		for (Funcionario funcionario : todosFuncionarios
 				.getColecaoFuncionarios()) {
-			
+
 			salarioFuncionario = SALARIO_DEFAULT + COMISSAO
 					* funcionario.getTotalDeVendas();
-			
-			folhaDePagamentoFuncionarios.put(funcionario.getNome(),
-					salarioFuncionario);
-			
+
+			salarioFuncionarios.put(funcionario.getNome(), salarioFuncionario);
+
 			despesas += salarioFuncionario;
 		}
 
-		/* Folha de Pagamento = HashMap com Salario dos Funcionarios,
-		 * double Quanto Tinha, double Despesas
+		/*
+		 * Folha de Pagamento = HashMap com Salario dos Funcionarios, double
+		 * Quanto Tinha, double Despesas
 		 */
 
-		Object[] folhaDePagamento = { folhaDePagamentoFuncionarios, caixaTotal,
-				despesas };
+		Object[] folhaDePagamento = { salarioFuncionarios, caixaTotal, despesas };
 
 		caixaTotal -= despesas;
+
+		pagouNesseMes[0] = new GregorianCalendar();
+		pagouNesseMes[1] = true;
+
 		return folhaDePagamento;
-		
+
 		// Fiz isso pra podermos usar tanto na GUI qto no Console :D
-		
+
 	}
+
+	/**
+	 * Metodo para atualizar as informacoes de Pagamento
+	 */
 
 	public void atualizaPagamento() {
 		GregorianCalendar dataUltimoPagamento = (GregorianCalendar) pagouNesseMes[0];
@@ -82,91 +108,81 @@ public class Sistema {
 			pagouNesseMes[1] = false;
 		}
 	}
+	
+	/* Getters e Setters */
 
 	/**
-	 * @return the pagouNesseMes
+	 * Metodo acessador das informacoes do Pagamento
+	 * 
+	 * @return Array contendo:<br>
+	 *         Calendar com a Data do Ultimo Pagamento<br>
+	 *         Boolean True se Ja Pagou esse mes e False se nao
 	 */
 	public Object[] getPagouNesseMes() {
 		return pagouNesseMes;
 	}
 
 	/**
-	 * @param pagouNesseMes
-	 *            the pagouNesseMes to set
-	 */
-	public void setPagouNesseMes(Object[] pagouNesseMes) {
-		this.pagouNesseMes = pagouNesseMes;
-	}
-
-	/**
-	 * @return the loginClientes
+	 * Metodo acessador dos Logins de Clientes
+	 * 
+	 * @return Hashmap contendo:<br>
+	 *         Hashmap<Login, Senha>
 	 */
 	public HashMap<String, String> getLoginClientes() {
 		return loginClientes;
 	}
 
 	/**
-	 * @return the loginFuncionarios
+	 * Metodo acessador dos Logins de Funcionarios
+	 * 
+	 * @return Hashmap contendo:<br>
+	 *         Hashmap<Login, Senha>
 	 */
 	public HashMap<String, String> getLoginFuncionarios() {
 		return loginFuncionarios;
 	}
 
 	/**
-	 * @return the todosImoveis
+	 * Metodo Acessador de Todos os Imoveis do Sistema
+	 * 
+	 * @return Colecao de Imoveis
 	 */
 	public ColecaoImoveis getTodosImoveis() {
 		return todosImoveis;
 	}
 
 	/**
-	 * @param todosImoveis
-	 *            the todosImoveis to set
-	 */
-	public void setTodosImoveis(ColecaoImoveis todosImoveis) {
-		this.todosImoveis = todosImoveis;
-	}
-
-	/**
-	 * @return the todosClientes
+	 * Metodo Acessador de Todos os Clientes do Sistema
+	 * 
+	 * @return Colecao de Clientes
 	 */
 	public ColecaoClientes getTodosClientes() {
 		return todosClientes;
 	}
 
 	/**
-	 * @param todosClientes
-	 *            the todosClientes to set
-	 */
-	public void setTodosClientes(ColecaoClientes todosClientes) {
-		this.todosClientes = todosClientes;
-	}
-
-	/**
-	 * @return the todosFuncionarios
+	 * Metodo Acessador de Todos os Funcionarios do Sistema
+	 * 
+	 * @return Colecao de Funcionarios
 	 */
 	public ColecaoFuncionario getTodosFuncionarios() {
 		return todosFuncionarios;
 	}
 
 	/**
-	 * @param todosFuncionarios
-	 *            the todosFuncionarios to set
-	 */
-	public void setTodosFuncionarios(ColecaoFuncionario todosFuncionarios) {
-		this.todosFuncionarios = todosFuncionarios;
-	}
-
-	/**
-	 * @return the caixaTotal
+	 * Metodo Acessador do Caixa Total do Sistema
+	 * 
+	 * @return Caixa Total
 	 */
 	public double getCaixaTotal() {
 		return caixaTotal;
 	}
 
 	/**
+	 * Metodo Modificador do Caixa Total
+	 * 
 	 * @param caixaTotal
-	 *            the caixaTotal to set
+	 *            Novo Caixa Total
 	 */
 	public void setCaixaTotal(double caixaTotal) {
 		this.caixaTotal = caixaTotal;
