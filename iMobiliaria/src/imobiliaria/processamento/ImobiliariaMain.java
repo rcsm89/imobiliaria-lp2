d8f250e29c8b9e3b2fdb50ec5689897285b19caa
yurifariasg <yurifariasg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-24 17:25:24 +0000

    Update do Ant

--- iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
+++ iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
@@ -87,12 +87,11 @@ public class ImobiliariaMain {
 	}
 
 	private static String promptPrincipal() {
-		return " * ----------"
-				+ "------------- * Imobiliaria * ----------------------- * \n"
+		return " * ----------------------- * Imobiliaria * ----------------------- * \n"
 				+ "1 - Administrador.\n"
 				+ "2 - Funcionario.\n"
-				+ "3 - Cliente.\n" 
-				+ "4 - Cadastrar Cliente.\n" 
+				+ "3 - Cliente.\n"
+				+ "4 - Cadastrar Cliente.\n"
 				+ "0 - Sair.\n"
 				+ "-------------Digite o numero da opcao desejada-------------"
 				+ "\n" + "Número da opção: ";
@@ -116,39 +115,34 @@ public class ImobiliariaMain {
 
 	/* Metodos de Administrador */
 
-
-	
-	/* Metodos de Administrador */
-
 	// SuppressWarnings pelo Cast do Object para HashMap
 	@SuppressWarnings("unchecked")
 	private static void opcoesAdmin() {
-		
+
 		boolean menuAdminRodando = true;
-		
+
 		System.out.println("\n" + "Menu de Administracao - iMobiliaria\n"
 				+ "1. Verificar e Alterar dados de Clientes\n"
 				+ "2. Verificar e Alterar dados de Funcionarios\n"
 				+ "3. Verificar e Alterar dados de Imoveis\n"
-				+ "4. Efetua pedido de Cliente\n"
-				+ "5. Pagamento de Contas\n"
+				+ "4. Efetua pedido de Cliente\n" + "5. Pagamento de Contas\n"
 				+ "6. Sair");
-		
+
 		int opcaoEscolhida = recebeInteiroEntre(1, 6);
 
 		switch (opcaoEscolhida) {
 
 		case 1:
 			// CRDU de Clientes
-			//crduClientes();
+			// crduClientes();
 			break;
 		case 2:
 			// CRDU de Funcioanrios
-			//crduFuncionarios();
+			// crduFuncionarios();
 			break;
 		case 3:
 			// CRDU de Imoveis
-			//crduImoveis();
+			// crduImoveis();
 			break;
 		case 4:
 			// Efetua pedido do Cliente
@@ -157,18 +151,18 @@ public class ImobiliariaMain {
 
 			// Se ele ja pagou esse Mes
 			Object[] folhaDePagamento;
-			
+
 			try {
-				
+
 				folhaDePagamento = sistema.efetuaPagamentoNoMes();
-				
+
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 				break;
 			}
 
 			HashMap<String, Double> pagamentoFuncionarios = (HashMap<String, Double>) folhaDePagamento[0];
-			
+
 			if (!(pagamentoFuncionarios.isEmpty())) {
 				System.out.println("Funcionario ---------- Salario");
 				for (String funcionario : pagamentoFuncionarios.keySet()) {
@@ -178,22 +172,23 @@ public class ImobiliariaMain {
 			} else {
 				System.out.println("Nenhum funcionario cadastrado");
 			}
-			
-			System.out.println("-------------------------------------------\n" +
-					"Total Antigo : " + (Double) folhaDePagamento[1] + "\n" +
-							"Despesas: " + (Double) folhaDePagamento[2] + "\n" +
-									"Novo Total: " + sistema.getCaixaTotal());
-			
+
+			System.out.println("-------------------------------------------\n"
+					+ "Total Antigo : " + (Double) folhaDePagamento[1] + "\n"
+					+ "Despesas: " + (Double) folhaDePagamento[2] + "\n"
+					+ "Novo Total: " + sistema.getCaixaTotal());
+
 			break;
 		case 6:
 			menuAdminRodando = false;
 			break;
 		}
-		
-		/* Se ele nao digitou para sair (opcao 6)
-		 * O menu eh chamado novamente (Chamada Recursiva)
+
+		/*
+		 * Se ele nao digitou para sair (opcao 6) O menu eh chamado novamente
+		 * (Chamada Recursiva)
 		 */
-		
+
 		if (menuAdminRodando) {
 			opcoesAdmin();
 		}
@@ -221,13 +216,11 @@ public class ImobiliariaMain {
 	private static void opcoesCliente() {
 		boolean menuClienteRodando = true;
 
-		System.out
-				.print("_____________Menu de Cliente__________ \n"
-						+ "1 - Listar Imoveis da Imobiliaria.\n"
-						+ "2 - Ver informacoes do cliente.\n"
-						+ "3 - Deslogar.\n"
-						+ "-------------Digite o numero da opcao desejada-------------"
-						+ "\n" + "Número da opção: ");
+		System.out.print("_____________Menu de Cliente__________ \n"
+				+ "1 - Listar Imoveis da Imobiliaria.\n"
+				+ "2 - Ver informacoes do cliente.\n" + "3 - Deslogar.\n"
+				+ "-------------Digite o numero da opcao desejada-------------"
+				+ "\n" + "Número da opção: ");
 
 		int opcaoEscolhida = recebeInteiroEntre(1, 3);
 
