524f5209e53a40089cdb013f5aa2dd011ce372db
yurifariasg <yurifariasg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-25 02:53:28 +0000

    Update no MAIN!

--- trunk/iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
+++ trunk/iMobiliaria/src/imobiliaria/processamento/ImobiliariaMain.java
@@ -1,8 +1,10 @@
 package imobiliaria.processamento;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.List;
 
 import imobiliaria.util.*;
 
@@ -44,7 +46,7 @@ public class ImobiliariaMain {
 				break;
 
 			case 2:
-				
+
 				System.out.print("Login: ");
 				String loginFunc = VerificaInvalido.recebeString();
 				System.out.print("Senha: ");
@@ -134,7 +136,7 @@ public class ImobiliariaMain {
 	 */
 
 	/* Metodos de Cliente */
-	
+
 	private static void opcoesCliente() {
 		boolean menuClienteRodando = true;
 
@@ -175,18 +177,77 @@ public class ImobiliariaMain {
 		}
 	}
 
-	
-	/* Metodos de Funcionario */ 
-	
+	private static void crduClientes() throws Exception {
+		boolean menuCrduCliente = true;
+		int opcaoCliente1 = 0;
+
+		System.out.println("----------------\n" + "1. Cadastrar um cliente\n"
+				+ "2. Verificar dados do cliente\n"
+				+ "3. Alterar dados do cliente\n" + "4. Sair\n"
+				+ "-------------Digite o numero da opcao desejada-------------"
+				+ "\n" + "Número da opção: ");
+
+		opcaoCliente1 = recebeInteiroEntre(1, 4);
+
+		switch (opcaoCliente1) {
+
+		case 1:
+
+			// cadastrar um cliente
+
+			System.out.println("--- Cadastrando um cliente ---\n");
+			System.out.print("Nome:");
+			String nome = recebeNome();
+			System.out.print("CPF:");
+			String cpf = recebeCpf();
+			System.out.print("Data de Nascimento (dd/MM/AAAA): ");
+			Calendar data = recebeData();
+			System.out.print("Endereco: ");
+			String endereco = recebeEndereco();
+			System.out.println("Qual sua preferência de imóvel?\n"
+					+ "1. Casa\n" + "2. Apartamento\n" + "3. Terreno\n"
+					+ "---------\n" + "Escolha: ");
+			TipoImovel pref = recebePreferencia();
+			Cliente novoCliente = new Cliente(cpf, data, endereco, nome, pref);
+			sistema.getTodosClientes().adicionaCliente(novoCliente);
+			System.out.println("Cliente Cadastrado!");
+			break;
+
+		case 2:
+			System.out.println("Verifica dados de cliente");
+
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			System.out.println("Voce foi deslogado\n");
+			menuCrduCliente = false;
+			break;
+
+		default:
+
+			System.out.println("\nDigite umas das opções sugeridas.");
+			break;
+
+		}
+		if (menuCrduCliente) {
+			crduClientes();
+		}
+	}
+
+	/* Metodos de Funcionario */
+
 	private static void opcoesFunc() throws Exception {
 
 		boolean menuFuncRodando = true;
 
-		System.out.println("\n" + "-----Menu de Funcionario - iMobiliaria-----\n"
+		System.out.println("\n"
+				+ "-----Menu de Funcionario - iMobiliaria-----\n"
 				+ "1. Cadastrar, Verificar e Alterar dados de Clientes\n"
 				+ "2. Cadastrar, Verificar e Alterar dados de Imoveis\n"
-				+ "3. Efetua pedido de Cliente\n"
-				+ "4. Minhas informações\n"
+				+ "3. Efetua pedido de Cliente\n" + "4. Minhas informações\n"
 				+ "5. Sair\n"
 				+ "-------------Digite o numero da opcao desejada-------------"
 				+ "\n" + "Número da opção: ");
@@ -198,17 +259,17 @@ public class ImobiliariaMain {
 		case 1:
 			crduClientes();
 			break;
-		
+
 		case 2:
-			//CRDUImoveis();
+			// CRDUImoveis();
 			break;
-			
+
 		case 3:
-			//efetuaPedido();
+			// efetuaPedido();
 			break;
-		
+
 		case 4:
-			//funcInfo();
+			// funcInfo();
 			break;
 
 		case 5:
@@ -216,15 +277,12 @@ public class ImobiliariaMain {
 			break;
 		}
 
-		
 		if (menuFuncRodando) {
 			opcoesFunc();
 		}
 	}
-	
 
 	/* Metodos de Administrador */
-
 
 	private static void opcoesAdmin() {
 
@@ -243,24 +301,24 @@ public class ImobiliariaMain {
 
 		case 1:
 			// CRDU de Clientes
-			//crduClientes();
+			// crduClientes();
 			break;
 		case 2:
 			// CRDU de Funcioanrios
-			//crduFuncionarios();
+			crduFuncionarios();
 			break;
 		case 3:
 			// CRDU de Imoveis
-//			crduImoveis();
+			// crduImoveis();
 			break;
 		case 4:
 			// Efetua pedido do Cliente
-//			efetuaPedido();
+			// efetuaPedido();
 			break;
 		case 5:
 
 			// Se ele ja pagou esse Mes
-		    pagamentoDoMes();
+			pagamentoDoMes();
 
 			break;
 		case 6:
@@ -277,7 +335,7 @@ public class ImobiliariaMain {
 			opcoesAdmin();
 		}
 	}
-	
+
 	// SuppressWarnings pelo Cast do Object para HashMap
 	@SuppressWarnings("unchecked")
 	private static void pagamentoDoMes() {
@@ -310,6 +368,171 @@ public class ImobiliariaMain {
 				+ "Novo Total: " + sistema.getCaixaTotal());
 	}
 
+	private static void crduFuncionarios() {
+		boolean continua = true;
+		System.out.println("Opcoes de Funcionarios ---\n"
+				+ "1. Cadastrar Funcionario\n"
+				+ "2. Modificar/Remover Funcionario por CPF"
+				+ "3. Listar funcionarios\n" + "4. Sair");
+		int opcao = recebeInteiroEntre(1, 4);
+
+		switch (opcao) {
+		case 1:
+
+			cadastrarFuncionario();
+
+			break;
+		case 2:
+			
+			try {
+			modificaFunc();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+
+			break;
+		case 3:
+
+			List<Funcionario> funcionarios = sistema.getTodosFuncionarios()
+					.getColecaoFuncionarios();
+
+			listagemDeFuncionario(funcionarios);
+
+			break;
+		case 4:
+			continua = false;
+			break;
+		}
+		if (continua) {
+			crduFuncionarios();
+		}
+	}
+
+	private static void cadastrarFuncionario() {
+		
+		// Fazer!!
+	}
+
+	private static void modificaFunc() throws Exception {
+
+		System.out.print("Digite o nome do Funcionario que deseja Modificar: ");
+		String nome = recebeNome();
+
+		List<Funcionario> funcionarios = sistema.getTodosFuncionarios()
+				.getFuncionarioPorNome(nome);
+
+		if (funcionarios.isEmpty()) {
+			System.out.println("Nenhum Funcionario com esse nome");
+			return;
+		}
+
+		Funcionario funcionario;
+
+		if (!(funcionarios.size() == 1)) {
+			int contador = 0;
+			for (Funcionario f : funcionarios) {
+				contador++;
+				System.out.println(contador + ". " + f.getCpf() + " - "
+						+ f.getNome() + " - " + f.getDataNascimento());
+			}
+			System.out.print("Numero do Funcionario que deseja Modificar: ");
+			int opcao = recebeInteiroEntre(1, contador);
+			funcionario = funcionarios.get(opcao - 1);
+		} else {
+			// So um funcionario na lista
+			funcionario = funcionarios.get(0);
+		}
+
+		/*
+		 * Pegou Funcionario, agora vamos listar opcoes para sua modificacao
+		 */
+
+		modificaFuncionario(funcionario);
+
+	}
+
+	private static void modificaFuncionario(Funcionario funcionario)
+			throws Exception {
+
+		boolean continua = true;
+
+		System.out.print("Informacoes de " + funcionario.getNome() + "\n"
+				+ "01. CRECI: " + funcionario.getCreci() + "\n" + "02. Nome: "
+				+ funcionario.getCreci() + "\n" + "03. Data de Nascimento: "
+				+ funcionario.getDataNascimento() + "\n" + "04. CPF: "
+				+ funcionario.getCpf() + "\n" + "05. Endereco: "
+				+ funcionario.getEndereco() + "\n"
+				+ "06. Valor Total de Vendas no Mes: "
+				+ funcionario.getTotalDeVendas() + "\n"
+				+ "07. Listar Vendas no Mes\n"
+				+ "08. Listar Historico de Vendas\n"
+				+ "09. Deleta Funcionario\n" + "10. Sair\n"
+				+ "Digite a opcao que deseja: ");
+
+		int opcao = recebeInteiroEntre(1, 10);
+
+		switch (opcao) {
+		case 2:
+			System.out.print("Digite o novo nome: ");
+			String nome = recebeNome();
+			funcionario.setNome(nome);
+
+			break;
+		case 3:
+
+			System.out.print("Digite a nova Data de Nascimento: ");
+			Calendar data = recebeData();
+			funcionario.setDataNascimento(data);
+
+			break;
+		case 5:
+
+			System.out.print("Digite o novo endereco: ");
+			String endereco = recebeEndereco();
+			funcionario.setEndereco(endereco);
+
+			break;
+		case 7:
+
+			ArrayList<Imovel> imoveisDoMes = funcionario
+					.getImoveisVendidosMes().getImoveis();
+
+			listagemDeImovel(imoveisDoMes);
+
+			break;
+		case 8:
+
+			ArrayList<Imovel> imoveisHistorico = funcionario
+					.getImoveisVendidos().getImoveis();
+
+			listagemDeImovel(imoveisHistorico);
+
+			break;
+		case 9:
+
+			sistema.getTodosFuncionarios().removeFuncionario(
+					funcionario.getCreci());
+			System.out.println("Funcionario Removido com Sucesso!");
+
+			continua = false;
+
+			break;
+		case 10:
+
+			continua = false;
+
+			break;
+		default:
+			System.out.println("Opcao nao pode ser modificada ou listada");
+		}
+
+		if (continua) {
+			modificaFuncionario(funcionario);
+		}
+
+	}
+
 	/* Metodos Auxiliares ou de Entrada */
 
 	private static int recebeInteiroEntre(int min, int max) {
@@ -328,11 +551,11 @@ public class ImobiliariaMain {
 		}
 		return numero;
 	}
-	
-	public static String recebeNome() {
-    	Scanner entrada = new Scanner(System.in);
 
-    	if (!entrada.hasNextLine()) {
+	public static String recebeNome() {
+		Scanner entrada = new Scanner(System.in);
+
+		if (!entrada.hasNextLine()) {
 			entrada.next();
 			System.out.println("Nome invalido! Digite novamente: ");
 			return recebeNome();
@@ -345,13 +568,12 @@ public class ImobiliariaMain {
 			return recebeNome();
 		}
 		return string;
-   }
+	}
 
-	
 	public static String recebeCpf() {
 		Scanner entrada = new Scanner(System.in);
 
-    	if (!entrada.hasNextLine()) {
+		if (!entrada.hasNextLine()) {
 			entrada.next();
 			System.out.println("Cpf invalido! Digite novamente: ");
 			return recebeCpf();
@@ -365,141 +587,121 @@ public class ImobiliariaMain {
 		}
 		return string;
 	}
-	
+
 	public static String recebeEndereco() {
 		Scanner entrada = new Scanner(System.in);
 
-	    	if (!entrada.hasNextLine()) {
-				entrada.next();
-				System.out.println("Endereco invalido! Digite novamente: ");
-				return recebeEndereco();
-			}
+		if (!entrada.hasNextLine()) {
+			entrada.next();
+			System.out.println("Endereco invalido! Digite novamente: ");
+			return recebeEndereco();
+		}
 
-			String string = entrada.nextLine();
+		String string = entrada.nextLine();
 
-			if (VerificaInvalido.endereco(string)) {
-				System.out.println("Endereco invalido! Digite novamente: ");
-				return recebeEndereco();
-			}
-			return string;
+		if (VerificaInvalido.endereco(string)) {
+			System.out.println("Endereco invalido! Digite novamente: ");
+			return recebeEndereco();
+		}
+		return string;
 	}
-	
-	public static TipoImovel recebePreferencia(){
+
+	public static TipoImovel recebePreferencia() {
 		TipoImovel pref = null;
 		int escolhaPref = recebeInteiroEntre(1, 3);
 
-		if (escolhaPref == 1){
+		if (escolhaPref == 1) {
 			pref = TipoImovel.CASA;
-			
-		}else if (escolhaPref == 2){
+
+		} else if (escolhaPref == 2) {
 			pref = TipoImovel.APARTAMENTO;
-		
-		}else if (escolhaPref == 3){
+
+		} else if (escolhaPref == 3) {
 			pref = TipoImovel.TERRENO;
 		}
-		
+
 		return pref;
 	}
-  
-    public static Calendar recebeData(){
+
+	public static Calendar recebeData() {
 		Scanner sc = new Scanner(System.in);
 		Calendar data = null;
-		
-		if (!sc.hasNextLine()){
+
+		if (!sc.hasNextLine()) {
 			sc.next();
 			System.out.println("Data invalida! Digite novamente: ");
 			return recebeData();
 		}
-		
+
 		String dataString = sc.nextLine();
-		if (dataString.isEmpty() || dataString.length() < 10 || dataString.length() > 10) {
+		if (dataString.isEmpty() || dataString.length() < 10
+				|| dataString.length() > 10) {
 			System.out.println("Data invalida! Digite novamente: ");
 			return recebeData();
 		}
-		
+
 		String[] d = dataString.split("/");
 		int dia = Integer.parseInt(d[0]);
 		int mes = Integer.parseInt(d[1]);
 		int ano = Integer.parseInt(d[2]);
-		
-		if (dia < 1 || dia > 31 || mes < 1 || mes > 12 ){
+
+		if (dia < 1 || dia > 31 || mes < 1 || mes > 12) {
 			System.out.println("Data invalida! Digite novamente: ");
 			return recebeData();
 		}
-		
-		data = new GregorianCalendar(ano, mes-1, dia);
-		if (VerificaInvalido.data(data)){
+
+		data = new GregorianCalendar(ano, mes - 1, dia);
+		if (VerificaInvalido.data(data)) {
 			System.out.println("Data invalida! Tente Novamente.");
 			return recebeData();
 		}
 		return data;
-		
+
 	}
 
-	private static void crduClientes() throws Exception{
-		boolean menuCrduCliente = true;
-		int opcaoCliente1 = 0;
-		
-		System.out.println("----------------\n" +
-				"1. Cadastrar um cliente\n" +
-				"2. Verificar dados do cliente\n" +
-				"3. Alterar dados do cliente\n" +
-				"4. Sair\n" +
-				"-------------Digite o numero da opcao desejada-------------" +
-				"\n" + "Número da opção: ");
-		
-		
-		opcaoCliente1 = recebeInteiroEntre(1,4);
-		
-		switch (opcaoCliente1) {
-		
-		case 1:
-			
-			//cadastrar um cliente
-			
-			System.out.println("--- Cadastrando um cliente ---\n");
-			System.out.print("Nome:");
-			String nome = recebeNome();
-			System.out.print("CPF:");
-			String cpf = recebeCpf();
-			System.out.print("Data de Nascimento (dd/MM/AAAA): ");
-			Calendar data = recebeData();
-			System.out.print("Endereco: ");
-			String endereco = recebeEndereco();
-			System.out.println("Qual sua preferência de imóvel?\n" +
-					"1. Casa\n" +
-					"2. Apartamento\n" +
-					"3. Terreno\n" +
-					"---------\n" +
-					"Escolha: ");
-			TipoImovel pref = recebePreferencia();
-			Cliente novoCliente = new Cliente(cpf, data, endereco, nome, pref);
-			sistema.getTodosClientes().adicionaCliente(novoCliente);
-			System.out.println("Cliente Cadastrado!");
-			break;
+	private static void listagemDeImovel(ArrayList<Imovel> array) {
 
-		case 2:
-			System.out.println("Verifica dados de cliente");
-
-			break;
-
-		case 3:
-			break;
-			
-		case 4:
-			System.out.println("Voce foi deslogado\n");
-			menuCrduCliente = false;
-			break;
-
-		default:
-
-			System.out.println("\nDigite umas das opções sugeridas.");
-			break;
-
+		if (array.isEmpty()) {
+			System.out.println("Nenhum Imovel para ser listado");
+			return;
 		}
-		if (menuCrduCliente) {
-			crduClientes();
+
+		for (Imovel i : array) {
+			System.out.println(i.getRegistroImovel() + " - " + i.getNome()
+					+ " - R$ " + i.getValor() + "\n" + "Estado: "
+					+ i.getEstadoDoImovel() + " - Tipo: " + i.getTipoDoImovel()
+					+ " - Tipo Contratual: " + i.getTipoContratual()
+					+ " - Area: " + i.getArea().getComprimento() + "x"
+					+ i.getArea().getLargura() + "\n");
 		}
 	}
-	
+
+	private static void listagemDeFuncionario(List<Funcionario> array) {
+
+		if (array.isEmpty()) {
+			System.out.println("Nada a ser listado");
+			return;
+		}
+
+		for (Pessoa p : array) {
+			System.out.println(p.getCpf() + " - " + p.getNome()
+					+ " - Data de Nascimento: " + p.getDataNascimento() + "\n"
+					+ "Endereco: " + p.getEndereco() + "\n");
+		}
+	}
+
+	private static void listagemDeClientes(List<Funcionario> array) {
+
+		if (array.isEmpty()) {
+			System.out.println("Nada a ser listado");
+			return;
+		}
+
+		for (Pessoa p : array) {
+			System.out.println(p.getCpf() + " - " + p.getNome()
+					+ " - Data de Nascimento: " + p.getDataNascimento() + "\n"
+					+ "Endereco: " + p.getEndereco() + "\n");
+		}
+	}
+
 }
