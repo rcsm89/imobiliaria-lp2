3b7ccec9e362bfdb316b31dfece0187402fe0093
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-05-28 23:16:07 +0000

    Main e InterfaceTextual finalmente no ar \o/

--- trunk/iMobiliaria/src/imobiliaria/userInterface/InterfaceTextual.java
+++ trunk/iMobiliaria/src/imobiliaria/userInterface/InterfaceTextual.java
@@ -3,6 +3,14 @@
  */
 package imobiliaria.userInterface;
 
+import imobiliaria.processamento.Sistema;
+import imobiliaria.processamento.TipoImovel;
+import imobiliaria.processamento.TipoLogin;
+import imobiliaria.util.MetodoEntrada;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+
 /**
  * Interface textual do sistema
  * 
@@ -12,74 +20,310 @@ package imobiliaria.userInterface;
  */
 public class InterfaceTextual {
 
-    private static String[] opcoesMenuCliente = { "Cadastrar Cliente",
-	    "Listar Clientes", "Excluir Cadastro de Cliente",
-	    "Atualizar Cadastro de Cliente" };
-    private static String[] opcoesMenuFuncionario = { "Cadastrar Funcionario",
-	    "Listar Funcionario", "Excluir Cadastro de Funcionario",
-	    "Atualizar Cadastro de Funcionario", "Cadastrar Imovel",
-	    "Listar Imoveis", "Excluir Cadastro de Imovel",
-	    "Atualizar Cadastro de Imovel"};
-    private static String[] opcoesMenuAdmin = { "" };
+    private Sistema sis;
+    private boolean loopPrincipal;
+    private String lineSep;
 
-    public static void promptMenuPrincipal() {
-	System.out
-		.println("--------------------- iMobiliaria ---------------------\n"
-			+ "  Bem Vindo, realize o login ou cadastre-se, por favor \n\n"
-			+ "1. Cliente                4. Cadastro\n"
-			+ "2. Funcionario            5. Sair\n"
-			+ "3. Administrador\n");
+    private final int CLIENTE = 1;
+    private final int FUNCIONARIO = 2;
+    private final int ADMINISTRADOR = 3;
+
+    // Mensagens do Sistema
+    private final String loginFail = "==================="
+	    + "  LOGIN FAILED =====================";
+
+    private final String programaFinalizado = "================= Programa "
+	    + "Finalizado =================";
+
+    private final String opcaoInvalida = "========= Digite Uma Opcao Valida"
+	    + ", Por Favor ==========";
+
+    /**
+     * Metodo que cria uma interface textual para o usuario
+     */
+    public InterfaceTextual() {
+	this.loopPrincipal = true;
+	this.lineSep = System.getProperty("line.separator");
+	this.sis = new Sistema();
     }
 
-    public static void promptMenuCliente() {
-	System.out
-		.println("----------------------- Cliente -----------------------\n\n"
-			+ constroeMenu(opcoesMenuCliente));
+    /**
+     * Interface textual do usuario com o programa.
+     */
+    public void interfaceComUsuario() {
 
-    }
+	final int CADASTRO_CLIENTE = 4;
+	final int SAIR = 5;
 
-    public static void promptMenuFuncionario() {
-	System.out
-		.println("--------------------- Funcionario ---------------------\n\n"
-			+ constroeMenu(opcoesMenuCliente)
-			+ "\n"
-			+ constroeMenu(opcoesMenuFuncionario));
-    }
+	while (loopPrincipal) {
+	    promptLogin();
+	    int opcao = MetodoEntrada.recebeInt();
 
-    public static void promptMenuAdmin() {
-	System.out
-		.println("-------------------- Administrador --------------------\n\n"
-			+ constroeMenu(opcoesMenuCliente)
-			+ "\n"
-			+ constroeMenu(opcoesMenuFuncionario)
-			+ "\n"
-			+ constroeMenu(opcoesMenuAdmin));
-    }
+	    switch (opcao) {
+	    case CLIENTE:
+		if (acesso(CLIENTE)) {
+		    interfaceCliente();
+		} else {
+		    System.out.println(loginFail);
+		}
+		break;
 
-    public static void promptCadastroCliente() {
-	System.out
-		.println("----------------- Cadastro de Cliente -----------------\n\n"
-			+ "");
-    }
+	    case FUNCIONARIO:
+		if (acesso(FUNCIONARIO)) {
+		    interfaceFuncionario();
+		} else {
+		    System.out.println(loginFail);
+		}
+		break;
 
-    public static void promptProgramaFinalizado() {
-	System.out
-		.println("-------------------------------------------------------\n\n"
-			+ "Programa Finalizado");
-    }
+	    case ADMINISTRADOR:
+		if (acesso(ADMINISTRADOR)) {
+		    interfaceAdmin();
+		} else {
+		    System.out.println(loginFail);
+		}
+		break;
 
-    public static void promptOpcaoInvalida() {
-	System.out
-		.println("-------------------------------------------------------\n"
-			+ "Digite uma opcao valida, por favor\n");
-    }
+	    case CADASTRO_CLIENTE:
+		cadastroDeClientes();
+		break;
 
-    private static String constroeMenu(String[] prompts) {
-	String opcoes = "";
-	for (String opcao : prompts) {
-	    opcoes += opcao + "\n";
+	    case SAIR:
+		setLoopPrincipal();
+		break;
+
+	    default:
+		System.out.println(opcaoInvalida);
+	    }
 	}
-	return opcoes;
+	System.out.println(programaFinalizado);
+	;
+    }
+
+    // Metodos da Interface
+
+    private void cadastroDeClientes() {
+
+	boolean repeteCadastro;
+	int opcaoImovel = 0;
+	do {
+	    System.out.println(lineSep
+		    + "----------------- Cadastro de Cliente -----------------"
+		    + lineSep);
+
+	    String nome = MetodoEntrada
+		    .recebeString("Digite o Nome do Cliente: ");
+
+	    String cpf = MetodoEntrada
+		    .recebeString("CPF (Apenas os 11 digitos): ");
+
+	    System.out.print("Data de Nascimento (dd/MM/AAAA): ");
+	    Calendar dataNascimento = MetodoEntrada.recebeData();
+
+	    String endereco = MetodoEntrada.recebeString("Endereco: ");
+
+	    System.out.print("\nQual sua preferência de imóvel?\n"
+		    + "1. Casa\n" + "2. Apartamento\n" + "3. Terreno\n"
+		    + "---------------\n");
+
+	    opcaoImovel = MetodoEntrada.recebeInt();
+
+	    TipoImovel preferencia;
+	    try {
+		preferencia = TipoImovel.values()[opcaoImovel];
+	    } catch (IndexOutOfBoundsException erro) {
+		preferencia = null;
+	    }
+	    try {
+		sis.controladorClientes().adicionaCliente(cpf, dataNascimento,
+			endereco, nome, preferencia);
+		repeteCadastro = false;
+	    } catch (Exception erro) {
+		System.out.println("\n=========== AVISO =============\n"
+			+ erro.getMessage());
+		repeteCadastro = true;
+	    }
+
+	} while (repeteCadastro);
+	System.out
+		.println("\n=========== Cadastro Efetuado com Sucesso =============\n"
+			+ "\nLogin default: 11 Digitos do seu CPF\nSenha default: "
+			+ "Data de Nascimento (DDmmAAAA)\n\n"
+			+ "        Voce podera redefinir suas preferencias\n"
+			+ "        posteriormente quando acessar sua conta\n");
+    }
+
+    private boolean acesso(int tipo) {
+
+	String userName = MetodoEntrada.recebeString("Login: ");
+	String password = MetodoEntrada.recebeString("Senha: ");
+
+	if (tipo == ADMINISTRADOR) {
+	    return sis.login(userName, password, TipoLogin.ADMINISTRADOR);
+
+	} else if (tipo == FUNCIONARIO) {
+	    return sis.login(userName, password, TipoLogin.FUNCIONARIO);
+
+	} else if (tipo == CLIENTE) {
+	    return sis.login(userName, password, TipoLogin.CLIENTE);
+
+	} else {
+	    return false;
+	}
+    }
+
+    private void interfaceCliente() {
+
+	boolean repeteMenu = true;
+	int opcao;
+
+	while (repeteMenu) {
+	    promptMenuCliente();
+	    opcao = MetodoEntrada.recebeInt();
+
+	    switch (opcao) {
+	    case 1:
+		break;
+
+	    }
+	}
+    }
+
+    private void interfaceFuncionario() {
+	boolean repeteMenu = true;
+	int opcao;
+
+	while (repeteMenu) {
+	    promptMenuFuncionario();
+	    opcao = MetodoEntrada.recebeInt();
+
+	    switch (opcao) {
+	    case 1:
+		break;
+
+	    }
+	}
+
+    }
+
+    private void interfaceAdmin() {
+	boolean repeteMenu = true;
+	int opcao;
+
+	while (repeteMenu) {
+	    promptMenuAdmin();
+	    opcao = MetodoEntrada.recebeInt();
+
+	    switch (opcao) {
+	    case 1:
+		break;
+
+	    }
+	}
+
+    }
+
+    // Metodos modificadores da Interface
+    private void setLoopPrincipal() {
+	loopPrincipal = !(loopPrincipal);
+    }
+
+    // Metodos que mostram o prompt da operacao atual
+    private void promptLogin() {
+	System.out
+		.println("--------------------- iMobiliaria ---------------------"
+			+ lineSep
+			+ "  Bem Vindo, realize o login ou cadastre-se, por favor "
+			+ lineSep
+			+ lineSep
+			+ "      1. Cliente                    4. Cadastro"
+			+ lineSep
+			+ "      2. Funcionario                5. Sair"
+			+ lineSep + "      3. Administrador" + lineSep);
+    }
+
+    private void promptMenuAdmin() {
+	System.out
+		.println("-------------------- Administrador --------------------"
+			+ lineSep + constroeMenu(opcoesMenu(ADMINISTRADOR)));
+    }
+
+    private void promptMenuCliente() {
+	System.out
+		.println("----------------------- Cliente -----------------------"
+			+ lineSep + constroeMenu(opcoesMenu(CLIENTE)));
+
+    }
+
+    private void promptMenuFuncionario() {
+	System.out
+		.println("--------------------- Funcionario ---------------------"
+			+ lineSep);
+	constroeMenu(opcoesMenu(FUNCIONARIO));
+
+    }
+
+    private LinkedList<String> opcoesMenu(int tipo) {
+
+	String[] opcoesMenuCliente = { "Cadastrar Cliente", "Listar Clientes",
+		"Excluir Cadastro de Cliente", "Atualizar Cadastro de Cliente" };
+	String[] opcoesMenuFuncionario = { "Cadastrar Funcionario",
+		"Listar Funcionario", "Excluir Cadastro de Funcionario",
+		"Atualizar Cadastro de Funcionario" };
+	String[] opcoesImoveis = { "Cadastrar Imovel", "Listar Imoveis",
+		"Excluir Cadastro de Imovel", "Atualizar Cadastro de Imovel" };
+	String[] opcoesMenuAdmin = { "Efetuar Pagamento" };
+
+	LinkedList<String> menu = new LinkedList<String>();
+
+	int numMenu = 1;
+	switch (tipo) {
+
+	case CLIENTE:
+	    for (String opcao : opcoesMenuCliente) {
+		opcao = numMenu + ". " + opcao + lineSep;
+		menu.add(opcao);
+		numMenu++;
+	    }
+	    break;
+
+	case FUNCIONARIO:
+	    menu = opcoesMenu(CLIENTE);
+	    numMenu = menu.size() + 1;
+
+	    String[][] conjuntoOpcoes = { opcoesMenuFuncionario, opcoesImoveis };
+
+	    for (String[] tipoDaVez : conjuntoOpcoes) {
+		menu.add(lineSep);
+		for (String opcao : tipoDaVez) {
+		    opcao = numMenu + ". " + opcao + lineSep;
+		    menu.add(opcao);
+		    numMenu++;
+		}
+	    }
+	    break;
+
+	case ADMINISTRADOR:
+	    menu = opcoesMenu(FUNCIONARIO);
+	    numMenu = menu.size() + 1;
+
+	    menu.add(lineSep);
+	    for (String opcao : opcoesMenuAdmin) {
+		opcao = numMenu + ". " + opcao + lineSep;
+		menu.add(opcao);
+		numMenu++;
+	    }
+	    break;
+	}
+	return menu;
+    }
+
+    private String constroeMenu(LinkedList<String> opcoes) {
+	String saida = "";
+	for (String opcao : opcoes) {
+	    saida += opcao;
+	}
+	return saida;
     }
 
 }
--- trunk/iMobiliaria/src/imobiliaria/userInterface/Main.java
+++ trunk/iMobiliaria/src/imobiliaria/userInterface/Main.java
@@ -1,7 +1,5 @@
 package imobiliaria.userInterface;
 
-import imobiliaria.util.MetodoEntrada;
-
 /**
  * Main do Sistema
  * 
@@ -11,60 +9,14 @@ import imobiliaria.util.MetodoEntrada;
  */
 public class Main {
 
+    /*
+     * Posteriormente sera criada uma classe GUI onde tudo ocorrera na mesma
+     * maneira. O main chamara um metodo principal, e tudo ficara a cargo da
+     * classe de interface
+     */
     public static void main(String[] args) {
 
-	final int MENU_CLIENTE = 1;
-	final int MENU_FUNCIONARIO = 2;
-	final int MENU_ADMINISTRADOR = 3;
-	final int CADASTRO_CLIENTE = 4;
-	final int SAIR = 5;
-
-	boolean loopMenuPrincipal = false;
-
-	do {
-
-	    InterfaceTextual.promptMenuPrincipal();
-	    int opcao = MetodoEntrada.recebeInt();
-
-	    switch (opcao) {
-
-	    case MENU_CLIENTE:
-		InterfaceTextual.promptMenuCliente();
-		int opcaoCliente = MetodoEntrada.recebeInt();
-		loopMenuPrincipal = false;
-		break;
-
-	    case MENU_FUNCIONARIO:
-		InterfaceTextual.promptMenuFuncionario();
-		int opcaoFuncionario = MetodoEntrada.recebeInt();
-		loopMenuPrincipal = false;
-		break;
-
-	    case MENU_ADMINISTRADOR:
-		InterfaceTextual.promptMenuAdmin();
-		int opcaoAdministrador = MetodoEntrada.recebeInt();
-		loopMenuPrincipal = false;
-		break;
-
-	    /*
-	     * Esse caso esta DELEGADO ao CONTROLADOR DE CLIENTE
-	     * Aqui as entradas sao Strings
-	     */
-	    case CADASTRO_CLIENTE:
-		InterfaceTextual.promptCadastroCliente();
-		loopMenuPrincipal = false;
-		break;
-
-	    case SAIR:
-		InterfaceTextual.promptProgramaFinalizado();
-		loopMenuPrincipal = false;
-		break;
-
-	    default:
-		InterfaceTextual.promptOpcaoInvalida();
-		loopMenuPrincipal = true;
-	    }
-
-	} while (loopMenuPrincipal);
+	InterfaceTextual ui = new InterfaceTextual();
+	ui.interfaceComUsuario();
     }
 }
