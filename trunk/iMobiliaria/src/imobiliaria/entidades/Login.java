050878382d96fdb534e654dceb403456daffbf0f
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-14 03:29:57 +0000

    Login com Javadoc Portugues/Ingles, verificado 100%

--- trunk/iMobiliaria/src/imobiliaria/entidades/Login.java
+++ trunk/iMobiliaria/src/imobiliaria/entidades/Login.java
@@ -1,25 +1,118 @@
+/*
+ * Por questões de seguranca, achei conveniente nao implementar ( por em quanto
+ * um metodo que modifica o tipo do login.
+ */
 package imobiliaria.entidades;
 
 import imobiliara.auxiliar.TipoLogin;
+import imobiliaria.util.VerificaInvalido;
 
+/**
+ * Login de acesso para uma conta do iMobiliaria<br>
+ * Access' login for an iMobiliaria's account
+ * 
+ * @author Jeanderson Barros Candido
+ * @version iteracao 2
+ * 
+ */
 public class Login {
 
     private TipoLogin tipoLogin;
     private String senha;
-    private String login;
+    private String userName;
 
-    public Login(String usrName, String password, TipoLogin tipo) {
+    /**
+     * Construtor de Login<br>
+     * Login's Constructor
+     * 
+     * @param nome
+     *            O user-name do Login<br>
+     *            The login's user-name
+     * @param password
+     *            A senha do Login<br>
+     *            The login's password
+     * @param tipo
+     *            O tipo do Login (Cliente, Funcionario, Admin)<br>
+     *            The login's type
+     * @throws Exception
+     *             se algum dos parametros forem invalidos<br>
+     *             if one of those parameters is invalid
+     */
+    public Login(String nome, String password, TipoLogin tipo) throws Exception {
+
+	if ((nome == null) || (password == null) || (tipo == null)) {
+	    throw new Exception("Message");
+	}
+	nome = nome.replace(" ", "");
+	if (VerificaInvalido.basico(nome) || password.isEmpty()) {
+	    throw new Exception("Message");
+	}
 	tipoLogin = tipo;
 	senha = password;
-	login = usrName;
+	userName = nome;
     }
 
+    /**
+     * Muda a senha de um Login<br>
+     * Changes the login's password
+     * 
+     * @param novaSenha
+     *            A nova senha a ser definida para o login<br>
+     *            The new login's password
+     */
     public void mudarSenha(String novaSenha) {
-	senha = novaSenha;
+	if (!(novaSenha == null)) {
+	    if (!novaSenha.isEmpty()) {
+		senha = novaSenha;
+	    }
+	}
     }
 
+    /**
+     * Muda o Username de um login<br>
+     * Changes the login's user-name
+     * 
+     * @param novoLogin
+     *            O novo username do login<br>
+     *            The new login's user-name
+     */
     public void mudarLogin(String novoLogin) {
-	login = novoLogin;
+	if (!(novoLogin == null || VerificaInvalido.basico(novoLogin))) {
+	    userName = novoLogin.replace(" ", "");
+	}
+    }
+
+    /**
+     * Retorna a senha do login<br>
+     * Returns the login's password
+     * 
+     * @return A senha<br>
+     *         The password
+     */
+    public String getSenha() {
+	return senha;
+    }
+
+    /**
+     * Retorna o Username de um login<br>
+     * Returns the login's user-name
+     * 
+     * @return O username de um login<br>
+     *         The login's user-name
+     */
+    public String getLogin() {
+	return userName;
+    }
+
+    /**
+     * Retorna o tipo do login<br>
+     * Return the login's type
+     * 
+     * @return O tipo do login<br>
+     *         The login's type
+     */
+    public TipoLogin getTipoLogin() {
+	return tipoLogin;
     }
 
     /*
@@ -33,6 +126,6 @@ public class Login {
 	    return false;
 	}
 	Login outroLogin = (Login) obj;
-	return login.equals(outroLogin);
+	return userName.equals(outroLogin.getLogin());
     }
 }
