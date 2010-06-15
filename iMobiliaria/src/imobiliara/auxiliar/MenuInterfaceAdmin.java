37abdde38b47cb653129915b02141dd5161bdce2
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-15 05:06:45 +0000

    RefactoringSeasons (LOL): Interface admin

---
+++ iMobiliaria/src/imobiliara/auxiliar/MenuInterfaceAdmin.java
@@ -0,0 +1,23 @@
+package imobiliara.auxiliar;
+
+/**
+ * Interface auxiliar para o menu do administrador<br>
+ * Auxiliary interface for a administrador's menu
+ * 
+ * @author Jeanderson Barros Candido
+ * @version version 02 (new)
+ * 
+ */
+public interface MenuInterfaceAdmin {
+
+    int ADMIN_CADASTRO_FUNCIONARIO = 11;
+    int ADMIN_LISTAR_FUNCIONARIO = 12;
+    int ADMIN_EXCLUIR_CADASTRO_FUNCIONARIO = 13;
+    int ADMIN_ATUALIZAR_CADASTRO_FUNCIONARIO = 14;
+    int ADMIN_VERIFICAR_INF_FUNCIONARIO = 15;
+    int ADMIN_EFETUAR_PEDIDO = 16;
+    int ADMIN_EFETUAR_PAGAMENTO = 17;
+    int ADMIN_VER_SALDO_ATUAL = 18;
+    int ADMIN_SAIR = 19;
+
+}
