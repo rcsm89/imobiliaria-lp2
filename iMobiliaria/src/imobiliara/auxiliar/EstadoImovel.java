ec3d5606eac2275172bbcac011f621a01d7a4d00
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-15 05:23:35 +0000

    javadoc inglês/portugues

--- iMobiliaria/src/imobiliara/auxiliar/EstadoImovel.java
+++ iMobiliaria/src/imobiliara/auxiliar/EstadoImovel.java
@@ -1,7 +1,15 @@
 package imobiliara.auxiliar;
 
+/**
+ * Classifica o estado de um imovel<br>
+ * Classifies the property's status
+ * 
+ * @author Jeanderson Barros Candido
+ * @version version 01 (update)
+ * 
+ */
 public enum EstadoImovel {
-	
-	A_VENDA, VENDIDO, ALUGADO, PEDIDO;
+
+    A_VENDA, VENDIDO, ALUGADO, PEDIDO;
 
 }
--- iMobiliaria/src/imobiliara/auxiliar/TipoImovel.java
+++ iMobiliaria/src/imobiliara/auxiliar/TipoImovel.java
@@ -1,5 +1,13 @@
 package imobiliara.auxiliar;
 
+/**
+ * Classifica o tipo de um imovel<br>
+ * Classifies the type of the property
+ * 
+ * @author Jeanderson Barros Candido
+ * @version version 01 (update)
+ * 
+ */
 public enum TipoImovel {
-	CASA, APARTAMENTO, TERRENO
+    CASA, APARTAMENTO, TERRENO
 }
