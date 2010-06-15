9adb58e7c41263b7ef4c19b37cdcddbc432ed403
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-15 07:38:59 +0000

    ZzZ...

--- trunk/iMobiliaria/src/imobiliaria/processamento/Sistema.java
+++ trunk/iMobiliaria/src/imobiliaria/processamento/Sistema.java
@@ -21,171 +21,161 @@ import imobiliaria.util.PersistenciaDados;
  */
 public class Sistema implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final String ARQUIVO_DO_SISTEMA = "DadosDeSistema.dat";
-	private final String ARQUIVO_DE_REGISTROS = "Registros.dat";
+    private final String ARQUIVO_DO_SISTEMA = "DadosDeSistema.dat";
+    private final String ARQUIVO_DE_REGISTROS = "Registros.dat";
 
-	private ControladorImovel controladorImoveis = ControladorImovel.getInstance();
-	private ControladorCliente controladorClientes = ControladorCliente.getInstance();
-	private ControladorFuncionario controladorFuncionarios = ControladorFuncionario.getInstance();
-	private ControladorPedidos controladorPedidos = ControladorPedidos.getInstance();
-	private ControladorTransacoes controladorTransacoes = ControladorTransacoes.getInstance();
-	private ControladorAlugueis controladorAlugueis = ControladorAlugueis.getInstance();
+    private ControladorImovel controladorImoveis = ControladorImovel
+	    .getInstance();
+    private ControladorCliente controladorClientes = ControladorCliente
+	    .getInstance();
+    private ControladorFuncionario controladorFuncionarios = ControladorFuncionario
+	    .getInstance();
+    private ControladorPedidos controladorPedidos = ControladorPedidos
+	    .getInstance();
+    private ControladorTransacoes controladorTransacoes = ControladorTransacoes
+	    .getInstance();
+    private ControladorAlugueis controladorAlugueis = ControladorAlugueis
+	    .getInstance();
+    private ControladorLogin controladorLogin = ControladorLogin.getInstance();
 
-	/**
-	 * Metodo Login - Efetua login baseado no login, senha e Tipo do Usuario
-	 * 
-	 * @param login
-	 *            Login do Usuario
-	 * @param senha
-	 *            Senha do Usuario
-	 * @param tipoDeUsuario
-	 *            Tipo de Usuario que vai ser logado
-	 * @return True - Se o usuario efetuou login com sucesso <br>
-	 *         False - Se o usuario nao conseguiu logar
-	 */
-	public boolean login(String login, String senha, TipoLogin tipoDeUsuario) {
+    /* Metodos Acessadores dos Controladores */
 
-		if (tipoDeUsuario == TipoLogin.ADMINISTRADOR) {
-			if (login.equals("admin") && senha.equals("admin"))
-				return true;
-			return false;
+    /**
+     * Metodo acessador do Controlador de Alugueis do Sistema
+     * 
+     * @return the controladorAlugueis
+     */
+    public ControladorAlugueis controladorAlugueis() {
+	return controladorAlugueis;
+    }
 
-		} else if (tipoDeUsuario == TipoLogin.FUNCIONARIO) {
+    /**
+     * Metodo acessador do Controlador de Logins do Sistema
+     * 
+     * @return the controladorLogin
+     */
+    public ControladorLogin getControladorLogin() {
+	return controladorLogin;
+    }
 
-			if (controladorFuncionarios.login(login, senha)) {
-				return true;
-			}
-			return false;
+    /**
+     * Metodo acessador do Contrador de Imoveis do Sistema
+     * 
+     * @return the controladorImoveis
+     */
+    public ControladorImovel controladorImoveis() {
+	return controladorImoveis;
+    }
 
-		} else {
+    /**
+     * Metodo acessador do Contrador de Clientes do Sistema
+     * 
+     * @return the controladorClientes
+     */
+    public ControladorCliente controladorClientes() {
+	return controladorClientes;
+    }
 
-			if (controladorClientes.login(login, senha)) {
-				return true;
-			}
-			return false;
-		}
-	}
+    /**
+     * Metodo acessador do Controlador de Pedidos do Sistema
+     * 
+     * @return the controladorPedidos
+     */
+    public ControladorPedidos controladorPedidos() {
+	return controladorPedidos;
+    }
 
-	/* Metodos Acessadores dos Controladores */
-	
-	/**
-	 * Metodo acessador do Controlador de Alugueis do Sistema
-	 * @return the controladorAlugueis
-	 */
-	public ControladorAlugueis controladorAlugueis() {
-		return controladorAlugueis;
-	}
+    /**
+     * Metodo acessador do Contrador de Funcionarios do Sistema
+     * 
+     * @return the controladorFuncionarios
+     */
+    public ControladorFuncionario controladorFuncionarios() {
+	return controladorFuncionarios;
+    }
 
-	/**
-	 * Metodo acessador do Contrador de Imoveis do Sistema
-	 * 
-	 * @return the controladorImoveis
-	 */
-	public ControladorImovel controladorImoveis() {
-		return controladorImoveis;
-	}
+    /**
+     * Metodo acessador do Contrador de Transacoes do Sistema
+     * 
+     * @return the controladorTransacoes
+     */
+    public ControladorTransacoes controladorTransacoes() {
+	return controladorTransacoes;
+    }
 
-	/**
-	 * Metodo acessador do Contrador de Clientes do Sistema
-	 * 
-	 * @return the controladorClientes
-	 */
-	public ControladorCliente controladorClientes() {
-		return controladorClientes;
-	}
+    /* Metodos de Atualizacao */
 
-	/**
-	 * Metodo acessador do Controlador de Pedidos do Sistema
-	 * 
-	 * @return the controladorPedidos
-	 */
-	public ControladorPedidos controladorPedidos() {
-		return controladorPedidos;
-	}
+    /**
+     * Metodo que atualiza os dados do sistema com dados gravados
+     * 
+     * @throws Exception
+     *             Lanca excecao caso os dados nao tenham sido gravados
+     *             anteriormente
+     */
+    public void atualizaDados() throws Exception {
 
-	/**
-	 * Metodo acessador do Contrador de Funcionarios do Sistema
-	 * 
-	 * @return the controladorFuncionarios
-	 */
-	public ControladorFuncionario controladorFuncionarios() {
-		return controladorFuncionarios;
-	}
-	
-	/**
-	 * Metodo acessador do Contrador de Transacoes do Sistema
-	 * 
-	 * @return the controladorTransacoes
-	 */
-	public ControladorTransacoes controladorTransacoes() {
-		return controladorTransacoes;
-	}
+	ControladorImovel.setInstance((ControladorImovel) PersistenciaDados
+		.ler(ARQUIVO_DO_SISTEMA));
+	ControladorCliente.setInstance((ControladorCliente) PersistenciaDados
+		.ler(ARQUIVO_DO_SISTEMA));
+	ControladorFuncionario
+		.setInstance((ControladorFuncionario) PersistenciaDados
+			.ler(ARQUIVO_DO_SISTEMA));
+	ControladorTransacoes
+		.setInstance((ControladorTransacoes) PersistenciaDados
+			.ler(ARQUIVO_DO_SISTEMA));
+	ControladorPedidos.setInstance((ControladorPedidos) PersistenciaDados
+		.ler(ARQUIVO_DO_SISTEMA));
+	ControladorAlugueis.setInstance((ControladorAlugueis) PersistenciaDados
+		.ler(ARQUIVO_DO_SISTEMA));
 
-	/* Metodos de Atualizacao */
+	ControladorLogin.setInstance(PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
 
-	/**
-	 * Metodo que atualiza os dados do sistema com dados gravados
-	 * 
-	 * @throws Exception
-	 *             Lanca excecao caso os dados nao tenham sido gravados
-	 *             anteriormente
-	 */
-	public void atualizaDados() throws Exception {
-		
-		
-		ControladorImovel.setInstance( (ControladorImovel)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		ControladorCliente.setInstance( (ControladorCliente)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		ControladorFuncionario.setInstance( (ControladorFuncionario)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		ControladorTransacoes.setInstance( (ControladorTransacoes)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		ControladorPedidos.setInstance( (ControladorPedidos)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		ControladorAlugueis.setInstance( (ControladorAlugueis)
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));
-		/*ControladorLogin.setInstance(
-				PersistenciaDados.ler(ARQUIVO_DO_SISTEMA));*/
-		
-		// Verificar necessidade!
-		
-		this.controladorImoveis = ControladorImovel.getInstance();
-		this.controladorClientes = ControladorCliente.getInstance();
-		this.controladorFuncionarios = ControladorFuncionario.getInstance();
-		this.controladorTransacoes = ControladorTransacoes.getInstance();
-		this.controladorPedidos = ControladorPedidos.getInstance();
-		this.controladorAlugueis = ControladorAlugueis.getInstance();
-		//this.controladorLogin = ControladorLogin.getInstance();
-		
-		controladorTransacoes.atualizaControlador();
-		
-		Imovel.setCriadorDeRegistro((Integer) PersistenciaDados
-				.ler(ARQUIVO_DE_REGISTROS));
-		Transacao.setCriadorRegistroTransacao((Integer) PersistenciaDados
-				.ler(ARQUIVO_DE_REGISTROS));
-	}
+	// Verificar necessidade!
 
-	/**
-	 * Metodo que salva os dados do sistema em um arquivo
-	 */
-	public void salvarDados() {
-		
-		controladorTransacoes.atualizaControlador();
-		
-		PersistenciaDados.gravar(ControladorImovel.getInstance(), ARQUIVO_DO_SISTEMA);
-		PersistenciaDados.gravar(ControladorCliente.getInstance(), ARQUIVO_DO_SISTEMA);
-		PersistenciaDados.gravar(ControladorFuncionario.getInstance(), ARQUIVO_DO_SISTEMA);
-		PersistenciaDados.gravar(ControladorTransacoes.getInstance(), ARQUIVO_DO_SISTEMA);
-		PersistenciaDados.gravar(ControladorPedidos.getInstance(), ARQUIVO_DO_SISTEMA);
-		PersistenciaDados.gravar(ControladorAlugueis.getInstance(), ARQUIVO_DO_SISTEMA);
-		//PersistenciaDados.gravar(ControladorLogin.getInstance(), ARQUIVO_DO_SISTEMA);
-		
-		PersistenciaDados.gravar(Imovel.getCriadorDeRegistro(),
-				ARQUIVO_DE_REGISTROS);
-		PersistenciaDados.gravar(Transacao.getCriadorRegistroTransacao(),
-				ARQUIVO_DE_REGISTROS);
-	}
+	this.controladorImoveis = ControladorImovel.getInstance();
+	this.controladorClientes = ControladorCliente.getInstance();
+	this.controladorFuncionarios = ControladorFuncionario.getInstance();
+	this.controladorTransacoes = ControladorTransacoes.getInstance();
+	this.controladorPedidos = ControladorPedidos.getInstance();
+	this.controladorAlugueis = ControladorAlugueis.getInstance();
+	this.controladorLogin = ControladorLogin.getInstance();
+
+	controladorTransacoes.atualizaControlador();
+
+	Imovel.setCriadorDeRegistro((Integer) PersistenciaDados
+		.ler(ARQUIVO_DE_REGISTROS));
+	Transacao.setCriadorRegistroTransacao((Integer) PersistenciaDados
+		.ler(ARQUIVO_DE_REGISTROS));
+    }
+
+    /**
+     * Metodo que salva os dados do sistema em um arquivo
+     */
+    public void salvarDados() {
+
+	controladorTransacoes.atualizaControlador();
+
+	PersistenciaDados.gravar(ControladorImovel.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorCliente.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorFuncionario.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorTransacoes.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorPedidos.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorAlugueis.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+	PersistenciaDados.gravar(ControladorLogin.getInstance(),
+		ARQUIVO_DO_SISTEMA);
+
+	PersistenciaDados.gravar(Imovel.getCriadorDeRegistro(),
+		ARQUIVO_DE_REGISTROS);
+	PersistenciaDados.gravar(Transacao.getCriadorRegistroTransacao(),
+		ARQUIVO_DE_REGISTROS);
+    }
 }
