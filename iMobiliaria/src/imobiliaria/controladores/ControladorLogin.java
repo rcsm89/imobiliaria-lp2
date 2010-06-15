649cc2ae3aab44cf5047d3ec32b62f3e591735fb
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-15 01:42:36 +0000

    Login Controller FULLY finalized

--- iMobiliaria/src/imobiliaria/controladores/ControladorLogin.java
+++ iMobiliaria/src/imobiliaria/controladores/ControladorLogin.java
@@ -1,26 +1,138 @@
+/*
+ * I need a way to work with files!
+ * Suggestions about it?
+ */
 package imobiliaria.controladores;
 
 import imobiliaria.entidades.Login;
 
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
+/**
+ * Gerenciamento de Logins do sistema<br>
+ * Manager the system's logins
+ * 
+ * @author Jeanderson Barros Candido
+ * @version iteracao 2
+ */
 public class ControladorLogin {
 
-    private Set<Login> loginsDoSistema;
+    // Attribute
+    private Collection<Login> loginsDoSistema;
 
+    // Constructor
+    /**
+     * Cria um Controlador de Logins<br>
+     * Creates a new Login's Controller
+     */
     public ControladorLogin() {
 	loginsDoSistema = new HashSet<Login>();
     }
 
+    // Methods
+
     /**
-     * Cadastra o login num repositorio de logins
+     * Retorna o numero de logins cadastrados<br>
+     * Returns how many logins was registered
      * 
-     * @param acc
-     *            Login a ser cadastrado
-     * @return
+     * @return Numero de logins cadastrados<br>
+     *         Number of registered logins
      */
-    public boolean cadastraLogin(Login acc) {
-	return loginsDoSistema.add(acc);
+    public int numLoginsCadastrados() {
+	return loginsDoSistema.size();
     }
+
+    // Register
+    /**
+     * Adiciona um novo login em ColecaoLogin<br>
+     * Adds a new login in ColecaoLogin
+     * 
+     * @param login
+     *            O login<br>
+     *            The login
+     * @return True se ColecaoLogins nao conter o login especificado<br>
+     *         True if the ColecaoLogins did not already contain the specified
+     *         login
+     */
+    public boolean adicionaLogin(Login login) {
+	if (!(login == null)) {
+	    for (Login lg : loginsDoSistema) {
+		if (lg.equals(login))
+		    return false;
+	    }
+	    return loginsDoSistema.add(login);
+	}
+	return false;
+    }
+
+    // Removes
+    /**
+     * Remove um login em ColecaoLogin<br>
+     * Removes a login from ColecaoLogin
+     * 
+     * @param login
+     *            A ser removido<br>
+     *            To be removed
+     * @return True se ColecaoLogin contem o login especificado<br>
+     *         True if ColecaoLogin contained the specified login
+     */
+    public boolean removeLogin(Login login) {
+	if (!(login == null)) {
+	    return loginsDoSistema.remove(login);
+	}
+	return false;
+    }
+
+    // Verify
+    /**
+     * Veririca se o login esta cadastrado<br>
+     * Verify if the login is registered
+     * 
+     * @param lg
+     *            O login a ser verificado<br>
+     *            The Login to be verified
+     * 
+     * @return True se o login estiver cadastradro<br>
+     *         True if the login has been registered
+     */
+    public boolean verificaLogin(Login lg) {
+	if (!(lg == null)) {
+	    return loginsDoSistema.contains(lg);
+	}
+	return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+	if (!(obj instanceof ControladorLogin))
+	    return false;
+	ControladorLogin outro = (ControladorLogin) obj;
+	return (outro.getCollection().containsAll(loginsDoSistema) && loginsDoSistema
+		.containsAll(outro.getCollection()));
+
+    }
+
+    /*
+     * Private access method. It isn't a good idea let this method public
+     * because we won't everyone using, and have total access to manager the
+     * logins
+     */
+
+    /**
+     * Retorna a colecao de login<br>
+     * Returns the login's collection
+     * 
+     * @return Uma colecao de logins<br>
+     *         A logins' collection
+     */
+    private Collection<Login> getCollection() {
+	return loginsDoSistema;
+    }
+
 }
