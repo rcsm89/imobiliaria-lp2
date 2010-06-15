27823ba17b3b5ebc475a0c39a17e12b407132e3b
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-15 05:05:17 +0000

    RefactoringSeasons (LOL): Interface para cliente

---
+++ trunk/iMobiliaria/src/imobiliara/auxiliar/MenuInterfaceCliente.java
@@ -0,0 +1,19 @@
+package imobiliara.auxiliar;
+
+/**
+ * Interface auxiliar para o menu de cliente<br>
+ * Auxiliary interface for a cliente's menu
+ * 
+ * @author Jeanderson Barros Candido
+ * @version version 02 (new)
+ * 
+ */
+public interface MenuInterfaceCliente {
+
+    final int CLIENTE_LISTAR_IMOVEIS = 1;
+    final int CLIENTE_FAZER_PEDIDO = 2;
+    final int CLIENTE_VERIF_DADOS = 3;
+    final int CLIENTE_HIST_COMPRAS = 4;
+    final int CLIENTE_SAIR = 5;
+
+}
