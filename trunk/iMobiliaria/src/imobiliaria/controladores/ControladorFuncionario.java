feeccab963656d05b8b71e033eae885076f718e2
thiagoferreirap09 <thiagoferreirap09@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-16 00:26:51 +0000

    ControladorFuncionario modificaFuncionario

--- trunk/iMobiliaria/src/imobiliaria/controladores/ControladorFuncionario.java
+++ trunk/iMobiliaria/src/imobiliaria/controladores/ControladorFuncionario.java
@@ -4,7 +4,13 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.TreeSet;
+
+import imobiliara.auxiliar.TipoContratual;
+import imobiliara.auxiliar.TipoImovel;
+import imobiliaria.entidades.Area;
 import imobiliaria.entidades.Funcionario;
+import imobiliaria.entidades.Imovel;
+import imobiliaria.exceptions.FuncionarioNotFoundException;
 import imobiliaria.processamento.ColecaoFuncionario;
 import imobiliaria.util.VerificaInvalido;
 
@@ -17,10 +23,9 @@ import imobiliaria.util.VerificaInvalido;
  */
 public class ControladorFuncionario implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private static ControladorFuncionario controladorFuncionarioUnico = new ControladorFuncionario();
-    private ColecaoFuncionario colecaoFunc = new ColecaoFuncionario();
-
+	private static final long serialVersionUID = 1L;
+	private static ControladorFuncionario controladorFuncionarioUnico = new ControladorFuncionario();
+	private ColecaoFuncionario colecaoFunc = new ColecaoFuncionario();
 
 	private ControladorFuncionario() {
 	}
@@ -33,220 +38,249 @@ public class ControladorFuncionario implements Serializable {
 	public static ControladorFuncionario getInstance() {
 		return controladorFuncionarioUnico;
 	}
-	
+
 	/**
 	 * Metodo que modifica a instancia unica do Controlador de Funcionario
 	 * 
 	 * @param controlador
 	 *            Nova Instancia para o Funcionario
 	 */
-	public static void setInstance(ControladorFuncionario controladorFuncionario){
-	
-		if (controladorFuncionario == null){
+	public static void setInstance(ControladorFuncionario controladorFuncionario) {
+
+		if (controladorFuncionario == null) {
 			throw new IllegalArgumentException("Controlador invalido");
 		}
 		controladorFuncionarioUnico = controladorFuncionario;
 	}
+
+	/**
+	 * Metodo acessador da colecao de funcionario
+	 * 
+	 * @return Colecao de funcionario
+	 */
+	public ColecaoFuncionario getColecaoFuncionario() {
+		return colecaoFunc;
+	}
+
+	/**
+	 * Adiciona um funcionario no Controlador
+	 * 
+	 * @param cpf
+	 *            Cpf do funcionario a ser adicionado
+	 * @param dataNascimento
+	 *            Data do nascimento do funcionario a ser adicionado
+	 * @param endereco
+	 *            Endereco do funcionario a ser adicionado
+	 * @param nome
+	 *            Nome do funcionario a ser adicionado
+	 * @param creci
+	 *            Creci do funcionario a ser adicionado
+	 * @return True, se um funcionario foi adicionado<br>
+	 *         False, caso contrario
+	 * @throws Exception
+	 *             Caso algum parametro seja invalido
+	 */
+
+	public boolean adicionaFuncionario(String cpf, Calendar dataNascimento,
+			String endereco, String nome, String creci) throws Exception {
+
+		Funcionario funcionarioASerAdicionado = new Funcionario(cpf,
+				dataNascimento, endereco, nome, creci);
+
+		return colecaoFunc.adicionaFuncionario(funcionarioASerAdicionado);
+	}
+
+	/**
+	 * Metodo responsavel por listar todos os funcionarios
+	 * 
+	 * @return Uma String contendo a lista dos funcionarios
+	 */
+	public String listaFuncionarios() {
+		return listaFuncionarios(colecaoFunc.getColecaoFuncionarios());
+	}
+
+	/**
+	 * Metodo responsavel por listar um conjunto de funcionario
+	 * 
+	 * @param conjuntoDeFuncionarios
+	 *            Representa um conjunto que contem os funcionarios que seram
+	 *            listados
+	 * @return Uma String contendo uma listagem dos funcionarios
+	 */
+	private String listaFuncionarios(TreeSet<Funcionario> listaDeFuncionarios) {
+		String saida = "";
+
+		for (Funcionario func : listaDeFuncionarios) {
+
+			saida += "Nome: " + func.getNome() + " - CPF: " + func.getCpf()
+					+ "\n" + "Endereco: " + func.getEndereco()
+					+ "\nData de Nascimento: " + func.getDataNascimento()
+					+ " - Creci: " + func.getCreci() + "\n\n";
+
+		}
+
+		return saida;
+	}
+
+	/**
+	 * Metodo que modifica um funcionario
+	 * 
+	 * @param creci
+	 *            Creci do funcionario
+	 * @param cpf
+	 *            Cpf do funcionario
+	 * @param dataNascimento
+	 *            Data do nascimento do funcionario
+	 * @param endereco
+	 *            Endereco do funcionario
+	 * @param nome
+	 *            Nome do funcionario
+	 * 
+	 * @throws Exception
+	 *             Se o funcionario for nulo
+	 */
+	public void modificaFuncionario(String creci, String cpf,
+			Calendar dataNascimento, String endereco, String nome)
+			throws Exception {
+		
+		Funcionario funcionario = colecaoFunc.getFuncionario(creci);
+		
+		if (funcionario == null){
+			throw new FuncionarioNotFoundException("Funcionario nao existente");
+		}
+
+		funcionario.setCpf(cpf);
+		funcionario.setDataNascimento(dataNascimento);
+		funcionario.setEndereco(endereco);
+		funcionario.setNome(nome);
+	}
 	
-    /**
-     * Metodo acessador da colecao de funcionario
-     * 
-     * @return Colecao de funcionario
-     */
-    public ColecaoFuncionario getColecaoFuncionario() {
-	return colecaoFunc;
-    }
+	/*
+	 * public void modificaImovel(String registro, String nome, String endereco,
+			double valor, Area area, TipoImovel tipoDoImovel,
+			TipoContratual tipoContratual) throws Exception {
 
-    /**
-     * Adiciona um funcionario no Controlador
-     * 
-     * @param cpf
-     *            Cpf do funcionario a ser adicionado
-     * @param dataNascimento
-     *            Data do nascimento do funcionario a ser adicionado
-     * @param endereco
-     *            Endereco do funcionario a ser adicionado
-     * @param nome
-     *            Nome do funcionario a ser adicionado
-     * @param creci
-     *            Creci do funcionario a ser adicionado
-     * @return True, se um funcionario foi adicionado<br>
-     *         False, caso contrario
-     * @throws Exception
-     *             Caso algum parametro seja invalido
-     */
+		int regImovel;
 
-    public boolean adicionaFuncionario(String cpf, Calendar dataNascimento,
-	    String endereco, String nome, String creci) throws Exception {
+		try {
+			regImovel = Integer.parseInt(registro);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Registro Invalido");
+		}
 
-	Funcionario funcionarioASerAdicionado = new Funcionario(cpf,
-		dataNascimento, endereco, nome, creci);
+		Imovel imovel = colecaoImovel.getImovelDeRegistro(regImovel);
 
-	return colecaoFunc.adicionaFuncionario(funcionarioASerAdicionado);
-    }
+		if (imovel == null)
+			throw new Exception("Imovel Nao Pertencente ao Controlador");
 
-    /**
-     * Metodo responsavel por listar todos os funcionarios
-     * 
-     * @return Uma String contendo a lista dos funcionarios
-     */
-    public String listaFuncionarios() {
-	return listaFuncionarios(colecaoFunc.getColecaoFuncionarios());
-    }
+		imovel.setNome(nome);
+		imovel.setEndereco(endereco);
+		imovel.setValor(valor);
+		imovel.setArea(area);
+		imovel.setTipoDoImovel(tipoDoImovel);
+		imovel.setTipoContratual(tipoContratual);
+	}
+	 */
+	/**
+	 * Imprime uma listagem com dados dos funcionarios
+	 * 
+	 * @param conjuntoFunc
+	 *            Conjunto de funcionarios a ser exibido
+	 */
+	public void imprimeListagemFuncionario(TreeSet<Funcionario> conjuntoFunc) {
 
-    /**
-     * Metodo responsavel por listar um conjunto de funcionario
-     * 
-     * @param conjuntoDeFuncionarios
-     *            Representa um conjunto que contem os funcionarios que seram
-     *            listados
-     * @return Uma String contendo uma listagem dos funcionarios
-     */
-    private String listaFuncionarios(TreeSet<Funcionario> listaDeFuncionarios) {
-	String saida = "";
+		if (conjuntoFunc.isEmpty()) {
+			System.out.println("Nada a ser listado");
+			return;
+		}
 
-	for (Funcionario func : listaDeFuncionarios) {
-
-	    saida += "Nome: " + func.getNome() + " - CPF: " + func.getCpf()
-		    + "\n" + "Endereco: " + func.getEndereco()
-		    + "\nData de Nascimento: " + func.getDataNascimento()
-		    + " - Creci: " + func.getCreci() + "\n\n";
-
+		for (Funcionario func : conjuntoFunc) {
+			System.out.println("Cpf: " + func.getCpf() + " - " + func.getNome()
+					+ "Creci: " + func.getCreci() + "\n - Data de Nascimento: "
+					+ func.getDataNascimento() + "\nEndereco: "
+					+ func.getEndereco() + "\n");
+		}
 	}
 
-	return saida;
-    }
+	/**
+	 * Retorna um mapa com o nome do funcionario e o total de vendas como chave
+	 * e valor
+	 * 
+	 * @return Mapa retornado
+	 */
+	public HashMap<String, Double> listaTotaisDeVendas() {
+		HashMap<String, Double> funcSalario = new HashMap<String, Double>();
+		double valorDeVendas;
 
-    /**
-     * Metodo que modifica um funcionario
-     * 
-     * @param funcionario
-     *            Funcionario a ser modificado
-     * @param cpf
-     *            Cpf do funcionario
-     * @param dataNascimento
-     *            Data do nascimento do funcionario
-     * @param endereco
-     *            Endereco do funcionario
-     * @param nome
-     *            Nome do funcionario
-     * @param creci
-     *            Creci do funcionario
-     * @throws Exception
-     *             Se o funcionario for nulo
-     */
-    public void modificaFuncionario(Funcionario funcionario, String cpf,
-	    Calendar dataNascimento, String endereco, String nome, String creci)
-	    throws Exception {
-	if (funcionario == null) {
-	    throw new Exception("Funcionario invalido");
-	}
-	funcionario.setCpf(cpf);
-	funcionario.setDataNascimento(dataNascimento);
-	funcionario.setEndereco(endereco);
-	funcionario.setNome(nome);
-    }
-
-    /**
-     * Imprime uma listagem com dados dos funcionarios
-     * 
-     * @param conjuntoFunc
-     *            Conjunto de funcionarios a ser exibido
-     */
-    public void imprimeListagemFuncionario(TreeSet<Funcionario> conjuntoFunc) {
-
-	if (conjuntoFunc.isEmpty()) {
-	    System.out.println("Nada a ser listado");
-	    return;
+		for (Funcionario funcionario : colecaoFunc.getColecaoFuncionarios()) {
+			funcionario.resetaImoveisVendidosMes();
+			valorDeVendas = funcionario.getTotalDeVendas();
+			funcSalario.put(funcionario.getNome(), valorDeVendas);
+		}
+		return funcSalario;
 	}
 
-	for (Funcionario func : conjuntoFunc) {
-	    System.out.println("Cpf: " + func.getCpf() + " - " + func.getNome()
-		    + "Creci: " + func.getCreci() + "\n - Data de Nascimento: "
-		    + func.getDataNascimento() + "\nEndereco: "
-		    + func.getEndereco() + "\n");
+	/**
+	 * Metodo Acessador de um Funcionario
+	 * 
+	 * @param creci
+	 *            Creci do Funcionario a ser retornado
+	 * @return Funcionario ou null, caso nao exista
+	 */
+	public Funcionario getFuncionarioPorCreci(String creci) {
+		for (Funcionario func : colecaoFunc.getColecaoFuncionarios()) {
+			if (func.getCreci().equals(creci))
+				return func;
+		}
+		return null;
 	}
-    }
 
-    /**
-     * Retorna um mapa com o nome do funcionario e o total de vendas como chave
-     * e valor
-     * 
-     * @return Mapa retornado
-     */
-    public HashMap<String, Double> listaTotaisDeVendas() {
-	HashMap<String, Double> funcSalario = new HashMap<String, Double>();
-	double valorDeVendas;
+	/**
+	 * Metodo que exibe informacoes de um Funcionario
+	 * 
+	 * @param creci
+	 *            Creci do Funcionario a ser exibido
+	 * @return String contendo informacoes do Funcionario
+	 */
+	public String exibeFuncionarioPorCreci(String creci) {
+		Funcionario func = getFuncionarioPorCreci(creci);
 
-	for (Funcionario funcionario : colecaoFunc.getColecaoFuncionarios()) {
-	    funcionario.resetaImoveisVendidosMes();
-	    valorDeVendas = funcionario.getTotalDeVendas();
-	    funcSalario.put(funcionario.getNome(), valorDeVendas);
+		return "Nome: " + func.getNome() + "\nCreci: " + func.getCreci()
+				+ "\nCpf: " + func.getCpf() + "\nData de Nascimento: "
+				+ func.getDataNascimento() + "\nEndereco: "
+				+ func.getEndereco();
 	}
-	return funcSalario;
-    }
 
-    /**
-     * Metodo Acessador de um Funcionario
-     * 
-     * @param creci
-     *            Creci do Funcionario a ser retornado
-     * @return Funcionario ou null, caso nao exista
-     */
-    public Funcionario getFuncionarioPorCreci(String creci) {
-	for (Funcionario func : colecaoFunc.getColecaoFuncionarios()) {
-	    if (func.getCreci().equals(creci))
-		return func;
+	/**
+	 * Metodo Acessador de um Funcionario
+	 * 
+	 * @param cpf
+	 *            CPF do Funcionario a ser retornado
+	 * @return Funcionario ou null, caso nao exista
+	 */
+	public Funcionario getFuncionarioPorCpf(String cpf) {
+		for (Funcionario func : colecaoFunc.getColecaoFuncionarios()) {
+			if (func.getCpf().equals(cpf))
+				return func;
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /**
-     * Metodo que exibe informacoes de um Funcionario
-     * 
-     * @param creci
-     *            Creci do Funcionario a ser exibido
-     * @return String contendo informacoes do Funcionario
-     */
-    public String exibeFuncionarioPorCreci(String creci) {
-	Funcionario func = getFuncionarioPorCreci(creci);
+	/**
+	 * Metodo que exibe informacoes de um Funcionario
+	 * 
+	 * @param cpf
+	 *            CPF do Funcionario a ser exibido
+	 * @return String contendo informacoes do Funcionario
+	 */
+	public String exibeFuncionarioPorCpf(String cpf) {
+		Funcionario func = getFuncionarioPorCpf(cpf);
 
-	return "Nome: " + func.getNome() + "\nCreci: " + func.getCreci()
-		+ "\nCpf: " + func.getCpf() + "\nData de Nascimento: "
-		+ func.getDataNascimento() + "\nEndereco: "
-		+ func.getEndereco();
-    }
-
-    /**
-     * Metodo Acessador de um Funcionario
-     * 
-     * @param cpf
-     *            CPF do Funcionario a ser retornado
-     * @return Funcionario ou null, caso nao exista
-     */
-    public Funcionario getFuncionarioPorCpf(String cpf) {
-	for (Funcionario func : colecaoFunc.getColecaoFuncionarios()) {
-	    if (func.getCpf().equals(cpf))
-		return func;
+		return "Nome: " + func.getNome() + "\nCreci: " + func.getCreci()
+				+ "\nCpf: " + func.getCpf() + "\nData de Nascimento: "
+				+ func.getDataNascimento() + "\nEndereco: "
+				+ func.getEndereco();
 	}
-	return null;
-    }
-
-    /**
-     * Metodo que exibe informacoes de um Funcionario
-     * 
-     * @param cpf
-     *            CPF do Funcionario a ser exibido
-     * @return String contendo informacoes do Funcionario
-     */
-    public String exibeFuncionarioPorCpf(String cpf) {
-	Funcionario func = getFuncionarioPorCpf(cpf);
-
-	return "Nome: " + func.getNome() + "\nCreci: " + func.getCreci()
-		+ "\nCpf: " + func.getCpf() + "\nData de Nascimento: "
-		+ func.getDataNascimento() + "\nEndereco: "
-		+ func.getEndereco();
-    }
 
 	/**
 	 * Remove um funcionario da colecao apartir do seu creci
@@ -269,6 +303,5 @@ public class ControladorFuncionario implements Serializable {
 		}
 		return false;
 	}
-
 
 }
