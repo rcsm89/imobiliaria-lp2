0d4641176f3fc529f035edce56f3d97e9fd42bd8
brunofpaiwa <brunofpaiwa@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-17 00:03:56 +0000

    Agora SIM! testando tudo que é possível ocorrer

--- trunk/iMobiliaria/src/imobiliaria/testes/ColecaoImovelTest.java
+++ trunk/iMobiliaria/src/imobiliaria/testes/ColecaoImovelTest.java
@@ -1,9 +1,12 @@
 package imobiliaria.testes;
 
+import imobiliara.auxiliar.EstadoImovel;
 import imobiliara.auxiliar.TipoContratual;
 import imobiliara.auxiliar.TipoImovel;
 import imobiliaria.entidades.Area;
 import imobiliaria.entidades.Imovel;
+import imobiliaria.exceptions.ImovelInvalidoException;
+import imobiliaria.exceptions.ValorInvalidoException;
 import imobiliaria.processamento.ColecaoImoveis;
 
 import java.util.ArrayList;
@@ -29,7 +32,8 @@ public class ColecaoImovelTest {
 
 		Imovel imovel2 = new Imovel(
 				"Apartamento a Venda!!!",
-				"Rua Fernando Luiz Henrique dos Santos Altiplano, num 2831, JP/PB, Ed. Java, apto 1300",
+				"Rua Fernando Luiz Henrique dos Santos Altiplano, num 2831," +
+				" JP/PB, Ed. Java, apto 1300",
 				25000, new Area(4, 6), TipoImovel.APARTAMENTO,
 				TipoContratual.VENDA);
 
@@ -43,7 +47,7 @@ public class ColecaoImovelTest {
 		try{
 			colecaoImovel.addImovel(imovel1);
 			Assert.assertEquals(1, colecaoImovel.getImoveis().size());
-		}catch(Exception e){
+		}catch(IllegalArgumentException e){
 			Assert.fail("Nao Deveria Cair Nesse catch");
 		}
 
@@ -51,7 +55,7 @@ public class ColecaoImovelTest {
 		try{
 			colecaoImovel.addImovel(imovel1);
 			Assert.fail("Deveria Lancar Excecao aqui");
-		}catch(Exception e){
+		}catch(ImovelInvalidoException e){
 			Assert.assertEquals("Imovel ja Existente", e.getMessage());
 		}
 		
@@ -60,7 +64,7 @@ public class ColecaoImovelTest {
 		try{
 			colecaoImovel.addImovel(imovel2);
 			colecaoImovel.addImovel(imovel3);
-		}catch(Exception e){
+		}catch(IllegalArgumentException e){
 			Assert.fail("Nao Deveria Cair Nesse catch");
 		}
 		
@@ -69,7 +73,7 @@ public class ColecaoImovelTest {
 		try{
 			colecaoImovel.addImovel(imovel2);
 			Assert.fail("Deveria Lancar Excecao aqui");
-		}catch(Exception e){
+		}catch(ImovelInvalidoException e){
 			Assert.assertEquals("Imovel ja Existente", e.getMessage());
 		}
 		
@@ -94,9 +98,102 @@ public class ColecaoImovelTest {
 
 		colecaoComparavel.add(imovel1);
 		colecaoComparavel.add(imovel2);
+		
+		//testando o getImoveisDeValor que recebe dois doubles representando
+		//o intervalo de preco.
 		Assert.assertEquals(colecaoComparavel, colecaoImovel.getImoveisDeValor(
 				0, 30000));
-
+		
+		try{
+			colecaoImovel.getImoveisDeValor(10, 0);
+			Assert.fail("Deveria Lancar Excecao aqui");
+		}catch(ValorInvalidoException e){
+			Assert.assertEquals("Intervalo Invalido", e.getMessage());
+		}
+		
+		try{
+			colecaoImovel.getImoveisDeValor(10, 10);
+			Assert.fail("Deveria Lancar Excecao aqui");
+		}catch(ValorInvalidoException e){
+			Assert.assertEquals("Intervalo Invalido", e.getMessage());
+		}
+		
+		//Testando o getImoveis por EstadoDeImovel
+		Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim" +
+				" Caroca, Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 (PEQUENA)|" +
+				"CASA|ALUGUEL|A_VENDA, 1|Apartamento a Venda!!!|" +
+				"Rua Fernando Luiz Henrique dos Santos Altiplano, num 2831," +
+				" JP/PB, Ed. Java, apto 1300|25000.0|4.0x6.0 (PEQUENA)|" +
+				"APARTAMENTO|VENDA|A_VENDA, 2|Terreno a venda no Altiplano!|" +
+				"Rua Algumacoisa de algo, no Altiplano, Num 9999, JP/PB|50000.0|" +
+				"15.0x7.0 (GRANDE)|TERRENO|VENDA|A_VENDA]",
+				colecaoImovel.getImoveis(EstadoImovel.A_VENDA).toString());
+		
+		Assert.assertEquals("[]", colecaoImovel.
+				getImoveis(EstadoImovel.ALUGADO).toString());
+		Assert.assertEquals("[]", colecaoImovel.
+				getImoveis(EstadoImovel.PEDIDO).toString());
+		Assert.assertEquals("[]", colecaoImovel.
+				getImoveis(EstadoImovel.VENDIDO).toString());
+		
+		imovel1.vendido();
+		
+		Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim Caroca," +
+				" Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 (PEQUENA)|CASA|" +
+				"ALUGUEL|VENDIDO]",
+				colecaoImovel.getImoveis(EstadoImovel.VENDIDO).toString());
+		
+		imovel1.alugado();
+		
+		Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim Caroca," +
+				" Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 (PEQUENA)|CASA|" +
+				"ALUGUEL|ALUGADO]",
+				colecaoImovel.getImoveis(EstadoImovel.ALUGADO).toString());
+		
+		imovel1.pedido();
+		
+		Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim Caroca," +
+				" Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 (PEQUENA)|CASA|" +
+				"ALUGUEL|PEDIDO]",
+				colecaoImovel.getImoveis(EstadoImovel.PEDIDO).toString());
+		
+		imovel1.a_venda();
+		
+		//Testando o getImoveis por Nome
+		
+		Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim" +
+				" Caroca, Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 (PEQUENA)|" +
+				"CASA|ALUGUEL|A_VENDA, 1|Apartamento a Venda!!!|" +
+				"Rua Fernando Luiz Henrique dos Santos Altiplano, num 2831," +
+				" JP/PB, Ed. Java, apto 1300|25000.0|4.0x6.0 (PEQUENA)|" +
+				"APARTAMENTO|VENDA|A_VENDA, 2|Terreno a venda no Altiplano!|" +
+				"Rua Algumacoisa de algo, no Altiplano, Num 9999, JP/PB|50000.0|" +
+				"15.0x7.0 (GRANDE)|TERRENO|VENDA|A_VENDA]",
+				colecaoImovel.getImoveis("a").toString());
+		
+		try{
+			colecaoImovel.getImoveis("");
+			Assert.fail("Deveria Lancar Excecao aqui");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals("Nome Invalido", e.getMessage());
+		}
+		
+		try{
+			colecaoImovel.getImoveis("     ");
+			Assert.fail("Deveria Lancar Excecao aqui");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals("Nome Invalido", e.getMessage());
+		}
+		
+		try{
+			Assert.assertEquals("[0|Casa imobiliada para Alugar|Rua Joaquim" +
+					" Caroca, Bodocongo, Num 2471, CG/PB|3500.0|4.0x6.0 " +
+					"(PEQUENA)|CASA|ALUGUEL|A_VENDA]", colecaoImovel.
+					getImoveis("Casa imobiliada para Alugar").toString());
+		}catch(Exception e){
+			Assert.fail("Nao Deveria Cair Nesse catch");
+		}
+		
 		colecaoComparavel.clear();
 
 		colecaoComparavel.add(imovel3);
@@ -114,20 +211,21 @@ public class ColecaoImovelTest {
 
 		Assert.assertEquals(imovel1, colecaoImovel.getImovelDeRegistro(0));
 
-		// Equals
-
 		try{
 			colecaoImovelParaCompararEquals.addImovel(imovel1);
-			Assert.assertEquals(1, colecaoImovelParaCompararEquals.getImoveis().
-					size());
+			
+			Assert.assertEquals(1, colecaoImovelParaCompararEquals.
+					getImoveis().size());
 			
 			colecaoImovelParaCompararEquals.addImovel(imovel2);
-			Assert.assertEquals(2, colecaoImovelParaCompararEquals.getImoveis().
-					size());
+			
+			Assert.assertEquals(2, colecaoImovelParaCompararEquals.
+					getImoveis().size());
 	
 			colecaoImovelParaCompararEquals.addImovel(imovel3);
-			Assert.assertEquals(3, colecaoImovelParaCompararEquals.getImoveis().
-					size());
+			Assert.assertEquals(3, colecaoImovelParaCompararEquals.
+					getImoveis().size());
+			
 		}catch(Exception e){
 			Assert.fail("Nao Deveria Cair Nesse catch");
 		}
@@ -135,7 +233,7 @@ public class ColecaoImovelTest {
 		try{
 			colecaoImovelParaCompararEquals.addImovel(null);
 			Assert.fail("Deveria Lancar Excecao aqui");
-		}catch(Exception e){
+		}catch(IllegalArgumentException e){
 			Assert.assertEquals("Imovel Invalido", e.getMessage());
 		}
 
@@ -173,11 +271,24 @@ public class ColecaoImovelTest {
 			Assert.assertEquals("Imovel Invalido", e.getMessage());
 		}
 		
-		Assert.assertEquals(false, colecaoImovel
-				.equals(colecaoImovelParaCompararEquals));
+		//Testando os getImoveis
+		
+		
+		//Testando equals das Colecoes
+		Assert.assertFalse(colecaoImovel.
+				equals(colecaoImovelParaCompararEquals));
 
-		Assert.assertEquals(false, colecaoImovelParaCompararEquals
-				.equals(colecaoImovel));
+		Assert.assertFalse(colecaoImovelParaCompararEquals.
+				equals(colecaoImovel));
+		
+		Assert.assertTrue(colecaoImovel.equals(colecaoImovel));
+		Assert.assertTrue(colecaoImovelParaCompararEquals.
+				equals(colecaoImovelParaCompararEquals));
+		
+		colecaoImovel.addImovel(imovel2);
+		colecaoImovel.addImovel(imovel3);		
+		
+		Assert.assertTrue(colecaoImovel.equals(colecaoImovelParaCompararEquals));
 		
 	}
 }
