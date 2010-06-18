89375f7700879cf55ce6b7ac794a80364c9fbd7d
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-18 18:34:11 +0000

    Funcionalidades de Cliente

--- trunk/iMobiliaria/src/imobiliaria/auxiliar/MenuInterfaceCliente.java
+++ trunk/iMobiliaria/src/imobiliaria/auxiliar/MenuInterfaceCliente.java
@@ -7,10 +7,19 @@ package imobiliaria.auxiliar;
  */
 public interface MenuInterfaceCliente {
 
+
 	final int CLIENTE_LISTAR_IMOVEIS = 1;
+
 	final int CLIENTE_FAZER_PEDIDO = 2;
-	final int CLIENTE_VERIF_DADOS = 3;
-	final int CLIENTE_HIST_COMPRAS = 4;
-	final int CLIENTE_SAIR = 5;
+	final int CLIENTE_LISTAR_PEDIDOS = 3;
+	final int CLIENTE_CANCELA_PEDIDO = 4;
+
+	final int CLIENTE_VERIF_DADOS = 5;
+	final int CLIENTE_HIST_COMPRAS = 6;
+
+	final int CLIENTE_LISTAR_ALUGUEIS = 7;
+	final int CLIENTE_CANCELAR_ALUGUEL = 8;
+
+	final int CLIENTE_SAIR = 9;
 
 }
