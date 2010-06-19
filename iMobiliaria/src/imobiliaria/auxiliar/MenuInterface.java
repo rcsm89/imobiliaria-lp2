ef69ac92db6bbde5bca1512243b145f351683a52
jbc.ufcg <jbc.ufcg@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-06-19 00:57:12 +0000

    Funcionalidades oka @_@"

--- iMobiliaria/src/imobiliaria/auxiliar/MenuInterface.java
+++ iMobiliaria/src/imobiliaria/auxiliar/MenuInterface.java
@@ -7,40 +7,52 @@ package imobiliaria.auxiliar;
  */
 public interface MenuInterface {
 
-	String lineSep = System.getProperty("line.separator");
+    String lineSep = System.getProperty("line.separator");
 
-	int ADMINISTRADOR = 3;
-	int FUNCIONARIO = 2;
-	int CLIENTE = 1;
+    int ADMINISTRADOR = 3;
+    int FUNCIONARIO = 2;
+    int CLIENTE = 1;
 
-	// Opcoes em comum de Funcionario e Administrador
+    // Opcoes em comum de Funcionario e Administrador
+    int CADASTRO_CLIENTE = 1;
+    int LISTAR_CLIENTE = 2;
+    int EXCLUIR_CLIENTE = 3;
+    int ATUALIZAR_CADASTRO_CLIENTE = 4;
+    int INFORMACOES_CLIENTE = 5;
+    int VER_HIST_DE_UM_CLIENTE = 6;
 
-	int ATUALIZAR_CADASTRO_CLIENTE = 4;
-	int ATUALIZAR_CADASTRO_IMOVEL = 9;
-	int EXCLUIR_CADASTRO_IMOVEL = 8;
-	int INFORMACOES_CLIENTE = 5;
-	int INFORMACOES_IMOVEL = 10;
-	int CADASTRO_CLIENTE = 1;
-	int CADASTRO_IMOVEL = 6;
-	int EXCLUIR_CLIENTE = 3;
-	int LISTAR_CLIENTE = 2;
-	int LISTAR_IMOVEL = 7;
+    int CADASTRO_IMOVEL = 7;
+    int LISTAR_IMOVEIS = 8;
+    int EXCLUIR_CADASTRO_IMOVEL = 9;
+    int ATUALIZAR_CADASTRO_IMOVEL = 10;
 
-	// Mensagens do Sistema
+    int INFORMACOES_IMOVEL = 11;
 
-	String programaFinalizado = "================= Programa "
-			+ "Finalizado =================";
+    int LISTAR_ALUGUEIS = 12;
+    int LISTAR_ALUGUEIS_DE_UM_CLIENTE = 13;
+    int CANCELAR_ALUGUEL = 14;
 
-	String opcaoInvalida = "========= Digite Uma Opcao Valida"
-			+ ", Por Favor ==========";
+    int LISTAR_PEDIDO = 15;
+    int LISTAR_PEDIDO_DE_UM_CLIENTE = 16;
+    int EFETUAR_UM_PEDIDO = 17;
+    int CANCELAR_UM_PEDIDO = 18;
 
-	String loginFail = "===================  LOGIN FAILED =="
-			+ "===================";
 
-	String loginOk = "=================== SEJA BEM VINDO ===="
-			+ "================";
+    // Mensagens do Sistema
 
-	String logOut = "================ VOCE SAIU DO SISTEMA =="
-			+ "===============";
+    String programaFinalizado = "================= Programa "
+	    + "Finalizado =================";
+
+    String opcaoInvalida = "========= Digite Uma Opcao Valida"
+	    + ", Por Favor ==========";
+
+    String loginFail = "===================  LOGIN FAILED =="
+	    + "===================";
+
+    String loginOk = "=================== SEJA BEM VINDO ===="
+	    + "================";
+
+    String logOut = "================ VOCE SAIU DO SISTEMA =="
+	    + "===============";
 
 }
--- iMobiliaria/src/imobiliaria/auxiliar/MenuInterfaceFuncionario.java
+++ iMobiliaria/src/imobiliaria/auxiliar/MenuInterfaceFuncionario.java
@@ -1,6 +1,5 @@
 package imobiliaria.auxiliar;
 
-
 /**
  * Interface auxiliar para o menu de funcionario
  * 
@@ -8,13 +7,8 @@ package imobiliaria.auxiliar;
  */
 public interface MenuInterfaceFuncionario {
 
-    // ISSO SERIA ADMIN NÃOOOOOOOO!??!?!?!?!?!?
-    // HistoricoDeVendasDeFuncionario(String creci)
-
-    final int FUNC_LISTAR_PEDIDOS = 11;
-    final int FUNC_EFETUAR_PEDIDO = 12;
-    final int FUNC_VERIFICAR_DADOS = 13;
-    final int FUNC_HIST_VENDAS = 14;
-    final int FUNC_SAIR = 15;
+    final int FUNC_VERIFICAR_DADOS = 19;
+    final int FUNC_HIST_VENDAS = 20;
+    final int FUNC_SAIR = 21;
 
 }
