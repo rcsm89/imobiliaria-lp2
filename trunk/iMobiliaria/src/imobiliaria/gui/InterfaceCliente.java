957f49a8a600911a82e030989e01796f7d7ac324
brunofpaiwa <brunofpaiwa@95a033c6-a0ff-3fdc-61ce-0bb4a52ca45e>
2010-07-04 19:07:53 +0000

    Mudança Gigante na cara da InterfaceCliente

--- trunk/iMobiliaria/src/imobiliaria/gui/InterfaceCliente.java
+++ trunk/iMobiliaria/src/imobiliaria/gui/InterfaceCliente.java
@@ -32,13 +32,15 @@ public class InterfaceCliente extends javax.swing.JFrame {
 
         JL_Cliente = new javax.swing.JLabel();
         JL_NomeDeCliente = new javax.swing.JLabel();
-        JB_VerificaPedidos = new javax.swing.JButton();
-        JB_VerificaAlugueis = new javax.swing.JButton();
-        JB_VerificaHistoricoCompras = new javax.swing.JButton();
-        JB_Desloga = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        JL_ImoveisCliente = new javax.swing.JList();
         JB_VerificaDadosCliente = new javax.swing.JButton();
+        JB_Desloga = new javax.swing.JButton();
+        JTP_PainelPedidAlugCompras = new javax.swing.JTabbedPane();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        JLI_PedidosCliente = new javax.swing.JList();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        JLI_AlugueisCliente = new javax.swing.JList();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        JLI_HistComprasCliente = new javax.swing.JList();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,28 +48,47 @@ public class InterfaceCliente extends javax.swing.JFrame {
 
         JL_NomeDeCliente.setText("- Nome Do Cliente Que Se Logou Ficará Aqui Entre Os Hífens -");
 
-        JB_VerificaPedidos.setText("Verificar Pedidos");
-        JB_VerificaPedidos.addActionListener(new java.awt.event.ActionListener() {
+        JB_VerificaDadosCliente.setText("Verificar Seus Dados");
+        JB_VerificaDadosCliente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JB_VerificaPedidosActionPerformed(evt);
+                JB_VerificaDadosClienteActionPerformed(evt);
             }
         });
 
-        JB_VerificaAlugueis.setText("Verificar Alugueis");
-
-        JB_VerificaHistoricoCompras.setText("Ver Histórico De Compras");
-
         JB_Desloga.setIcon(new javax.swing.ImageIcon("/home/bruno/workspacelcc3/iMobiliaria/src/imobiliaria/images/exitIcon.png")); // NOI18N
         JB_Desloga.setText("Deslogar");
+        JB_Desloga.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_DeslogaActionPerformed(evt);
+            }
+        });
 
-        JL_ImoveisCliente.setModel(new javax.swing.AbstractListModel() {
+        JLI_PedidosCliente.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane1.setViewportView(JL_ImoveisCliente);
+        jScrollPane3.setViewportView(JLI_PedidosCliente);
 
-        JB_VerificaDadosCliente.setText("Verificar Seus Dados");
+        JTP_PainelPedidAlugCompras.addTab("Pedidos", jScrollPane3);
+
+        JLI_AlugueisCliente.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(JLI_AlugueisCliente);
+
+        JTP_PainelPedidAlugCompras.addTab("Alugueis", jScrollPane2);
+
+        JLI_HistComprasCliente.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(JLI_HistComprasCliente);
+
+        JTP_PainelPedidAlugCompras.addTab("Histórico De Compras", jScrollPane1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,28 +100,19 @@ public class InterfaceCliente extends javax.swing.JFrame {
                         .addGap(296, 296, 296)
                         .addComponent(JL_Cliente))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(JB_VerificaDadosCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(31, 31, 31)
-                                .addComponent(JB_Desloga, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(JB_VerificaHistoricoCompras, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(JB_VerificaAlugueis, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(JB_VerificaPedidos, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE)))
-                        .addGap(69, 69, 69)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(101, 101, 101)
-                        .addComponent(JL_NomeDeCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 493, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(JL_NomeDeCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 493, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(JB_Desloga, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(169, 169, 169)
+                        .addComponent(JB_VerificaDadosCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+                        .addGap(36, 36, 36)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(JTP_PainelPedidAlugCompras, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
+                .addGap(31, 31, 31))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -109,29 +121,25 @@ public class InterfaceCliente extends javax.swing.JFrame {
                 .addComponent(JL_Cliente)
                 .addGap(18, 18, 18)
                 .addComponent(JL_NomeDeCliente)
-                .addGap(32, 32, 32)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(JB_VerificaPedidos, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(JB_VerificaAlugueis, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(JB_VerificaHistoricoCompras, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(JB_VerificaDadosCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 94, Short.MAX_VALUE)
-                        .addComponent(JB_Desloga, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(16, 16, 16))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 384, Short.MAX_VALUE)
-                        .addContainerGap())))
+                .addGap(18, 18, 18)
+                .addComponent(JTP_PainelPedidAlugCompras, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JB_VerificaDadosCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(JB_Desloga, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(16, 16, 16))
         );
 
         pack();
     }// </editor-fold>
 
-    private void JB_VerificaPedidosActionPerformed(java.awt.event.ActionEvent evt) {
-        // TODO add your handling code here:
+    private void JB_VerificaDadosClienteActionPerformed(java.awt.event.ActionEvent evt) {
+    	//abre uma nova janela contendo os dados do cliente
+    }
+
+    private void JB_DeslogaActionPerformed(java.awt.event.ActionEvent evt) {
+        new TelaPrincipal().setVisible(true);
+        dispose();
     }
 
     /**
@@ -147,14 +155,16 @@ public class InterfaceCliente extends javax.swing.JFrame {
 
     // Variables declaration - do not modify
     private javax.swing.JButton JB_Desloga;
-    private javax.swing.JButton JB_VerificaAlugueis;
     private javax.swing.JButton JB_VerificaDadosCliente;
-    private javax.swing.JButton JB_VerificaHistoricoCompras;
-    private javax.swing.JButton JB_VerificaPedidos;
+    private javax.swing.JList JLI_AlugueisCliente;
+    private javax.swing.JList JLI_HistComprasCliente;
+    private javax.swing.JList JLI_PedidosCliente;
     private javax.swing.JLabel JL_Cliente;
-    private javax.swing.JList JL_ImoveisCliente;
     private javax.swing.JLabel JL_NomeDeCliente;
+    private javax.swing.JTabbedPane JTP_PainelPedidAlugCompras;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     // End of variables declaration
 
     //JL_NomeDeCliente = Cliente.getNome();
